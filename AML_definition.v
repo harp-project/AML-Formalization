7295f1c4f00e6ed5f28ad4735cab67c5aeabbd51
Boros Attila Péter <attila9778@yahoo.com>
2019-09-24 16:52:01 +0200

    positiveness check implemented

--- AML_definition.v
+++ AML_definition.v
@@ -1,9 +1,10 @@
 Require Import String.
+
 Inductive EVar : Set := evar_c (id : string).
 Inductive SVar : Set := svar_c (id : string).
 Inductive Sigma : Set := sigma_c (id : string).
 
-Inductive Sigma_pattern :=
+Inductive Sigma_pattern : Set :=
   | sp_var (x : EVar)
   | sp_set (X : SVar)
   | sp_const (sigma : Sigma)
@@ -15,25 +16,45 @@ Inductive Sigma_pattern :=
 
 Definition evar_eq_dec : forall (x y : EVar), { x = y } + { x <> y }.
 Proof.
-decide equality.
-exact (string_dec id id0).
+  decide equality.
+  exact (string_dec id id0).
 Defined.
 
 Definition svar_eq_dec : forall (x y : SVar), { x = y } + { x <> y }.
 Proof.
-decide equality.
-exact (string_dec id id0).
+  decide equality.
+  exact (string_dec id id0).
 Defined.
 
 Definition sigma_eq_dec : forall (x y : Sigma), { x = y } + { x <> y }.
 Proof.
-decide equality.
-exact (string_dec id id0).
+  decide equality.
+  exact (string_dec id id0).
 Defined.
+
+Fixpoint negation_count_impl (X : SVar) (phi : Sigma_pattern) (count : nat) : nat :=
+  match phi with
+   | sp_var x => 0
+   | sp_set Y => if svar_eq_dec X Y then count else 0
+   | sp_const sigma => 0
+   | sp_app phi1 phi2 => (negation_count_impl X phi1 count) + (negation_count_impl X phi2 count)
+   | sp_bottom => 0
+   | sp_impl phi1 phi2 => (negation_count_impl X phi1 (count + 1)) + (negation_count_impl X phi2 count)
+   | sp_exists x phi' => negation_count_impl X phi' count
+(*    | sp_mX_dot_phi Y phi' => negation_count_impl X phi' count *)
+  end
+.
+
+Fixpoint negation_count (X : SVar) (phi : Sigma_pattern) : nat := (negation_count_impl X phi 0).
+
+Definition is_positive (X : SVar) (phi : Sigma_pattern) := Nat.even (negation_count X phi).
+(* Notation "t is_positive_in X" := (is_positive X t) (at level 90, left associativity). *)
+
+(* -- felfele es lefele akkumulator valtozo bevezetese *)
 
 Definition sp_eq_dec : forall (x y : Sigma_pattern), { x = y } + { x <> y }.
 Proof.
-decide equality.
+  decide equality.
   - exact (evar_eq_dec x0 x1).
   - exact (svar_eq_dec X X0).
   - exact (sigma_eq_dec sigma sigma0).
@@ -49,16 +70,16 @@ Definition sp_forall (x : EVar) (phi : Sigma_pattern) :=
   sp_not (sp_exists x (sp_not phi)).
 (*nuX.phi*)
 
-Fixpoint eSubstitution (phi : Sigma_pattern) (psi : Sigma_pattern) (x : EVar)
-  := match phi with
- | sp_var x' => if evar_eq_dec x x' then psi else sp_var x'
- | sp_set X => sp_set X
- | sp_const sigma => sp_const sigma
- | sp_app phi1 phi2 => sp_app (eSubstitution phi1 psi x)
-                              (eSubstitution phi2 psi x)
- | sp_bottom => sp_bottom
- | sp_impl phi1 phi2 => sp_impl (eSubstitution phi1 psi x)
+Fixpoint eSubstitution (phi : Sigma_pattern) (psi : Sigma_pattern) (x : EVar) := 
+  match phi with
+   | sp_var x' => if evar_eq_dec x x' then psi else sp_var x'
+   | sp_set X => sp_set X
+   | sp_const sigma => sp_const sigma
+   | sp_app phi1 phi2 => sp_app (eSubstitution phi1 psi x)
                                 (eSubstitution phi2 psi x)
- | sp_exists x' phi' => sp_exists x' (eSubstitution phi' psi x)
-end.
-
+   | sp_bottom => sp_bottom
+   | sp_impl phi1 phi2 => sp_impl (eSubstitution phi1 psi x)
+                                  (eSubstitution phi2 psi x)
+   | sp_exists x' phi' => sp_exists x' (eSubstitution phi' psi x)
+  end
+.
