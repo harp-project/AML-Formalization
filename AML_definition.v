63f51ee7902c4af5ec1b8a85edfd3536c27503ae
Balazs Piszkor <piszkorb@gmail.com>
2019-09-30 20:25:13 +0200

    AML:DEF Strictly positiveness definition

--- AML_definition.v
+++ AML_definition.v
@@ -12,7 +12,8 @@ Inductive Sigma_pattern : Set :=
   | sp_bottom
   | sp_impl (phi1 phi2 : Sigma_pattern)
   | sp_exists (x : EVar) (phi : Sigma_pattern)
-  (*| sp_ muX.phi?????*).
+  | sp_muXphi (X : SVar) (phi : Sigma_pattern).
+
 
 Definition evar_eq_dec : forall (x y : EVar), { x = y } + { x <> y }.
 Proof.
@@ -32,25 +33,20 @@ Proof.
   exact (string_dec id id0).
 Defined.
 
-Fixpoint negation_count_impl (X : SVar) (phi : Sigma_pattern) (count : nat) : nat :=
-  match phi with
-   | sp_var x => 0
-   | sp_set Y => if svar_eq_dec X Y then count else 0
-   | sp_const sigma => 0
-   | sp_app phi1 phi2 => (negation_count_impl X phi1 count) + (negation_count_impl X phi2 count)
-   | sp_bottom => 0
-   | sp_impl phi1 phi2 => (negation_count_impl X phi1 (count + 1)) + (negation_count_impl X phi2 count)
-   | sp_exists x phi' => negation_count_impl X phi' count
-(*    | sp_mX_dot_phi Y phi' => negation_count_impl X phi' count *)
-  end
-.
 
-Fixpoint negation_count (X : SVar) (phi : Sigma_pattern) : nat := (negation_count_impl X phi 0).
+Fixpoint spos_accumulated (phi : Sigma_pattern) (X : SVar) (nc : nat) : bool :=
+match phi with
+ | sp_var x => true
+ | sp_set Y => if (svar_eq_dec Y X) then (Nat.even nc) else false
+ | sp_const sigma => true
+ | sp_app phi1 phi2 => andb (spos_accumulated phi1 X nc) (spos_accumulated phi2 X nc)
+ | sp_bottom => true
+ | sp_impl phi1 phi2 => andb (spos_accumulated phi1 X (S nc)) (spos_accumulated phi2 X nc)
+ | sp_exists x phi => spos_accumulated phi X nc
+ | sp_muXphi Y phi => spos_accumulated phi X nc (* name colision *)
+end.
 
-Definition is_positive (X : SVar) (phi : Sigma_pattern) := Nat.even (negation_count X phi).
-(* Notation "t is_positive_in X" := (is_positive X t) (at level 90, left associativity). *)
-
-(* -- felfele es lefele akkumulator valtozo bevezetese *)
+Fixpoint strictly_positive (phi : Sigma_pattern) (X : SVar) : bool :=  spos_accumulated phi X 0.
 
 Definition sp_eq_dec : forall (x y : Sigma_pattern), { x = y } + { x <> y }.
 Proof.
@@ -59,6 +55,7 @@ Proof.
   - exact (svar_eq_dec X X0).
   - exact (sigma_eq_dec sigma sigma0).
   - exact (evar_eq_dec x0 x1).
+  - exact (svar_eq_dec X X0).
 Defined.
 
 Definition sp_not (phi : Sigma_pattern) := sp_impl phi sp_bottom.
@@ -77,9 +74,7 @@ Fixpoint eSubstitution (phi : Sigma_pattern) (psi : Sigma_pattern) (x : EVar) :=
    | sp_const sigma => sp_const sigma
    | sp_app phi1 phi2 => sp_app (eSubstitution phi1 psi x)
                                 (eSubstitution phi2 psi x)
-   | sp_bottom => sp_bottom
-   | sp_impl phi1 phi2 => sp_impl (eSubstitution phi1 psi x)
-                                  (eSubstitution phi2 psi x)
-   | sp_exists x' phi' => sp_exists x' (eSubstitution phi' psi x)
-  end
-.
+ | sp_exists x' phi' => sp_exists x' (eSubstitution phi' psi x)
+ | sp_muXphi X phi' => sp_muXphi X (eSubstitution phi' psi x)
+end.
+
