524ddf49410e55f2ba2ddd5569f88a592b90223a
piszkor <piszkorb@gmail.com>
2019-10-23 15:16:48 +0200

    Positiveness piszkor (#1)
    * AML:DEF Strictly positiveness definition
    
    * Update AML_definition.v
    
    Name collision fix
    
    * sp_tests

--- AML_definition.v
+++ AML_definition.v
@@ -12,7 +12,7 @@ Inductive Sigma_pattern : Set :=
   | sp_bottom
   | sp_impl (phi1 phi2 : Sigma_pattern)
   | sp_exists (x : EVar) (phi : Sigma_pattern)
-  (*| sp_ muX.phi?????*).
+  | sp_mu (X : SVar) (phi : Sigma_pattern).
 
 Definition evar_eq_dec : forall (x y : EVar), { x = y } + { x <> y }.
 Proof.
@@ -32,25 +32,23 @@ Proof.
   exact (string_dec id id0).
 Defined.
 
-Fixpoint negation_count_impl (X : SVar) (phi : Sigma_pattern) (count : nat) : nat :=
+Fixpoint spos_accumulated (phi : Sigma_pattern) (X : SVar) (nc : nat) : bool :=
   match phi with
-   | sp_var x => 0
-   | sp_set Y => if svar_eq_dec X Y then count else 0
-   | sp_const sigma => 0
-   | sp_app phi1 phi2 => (negation_count_impl X phi1 count) + (negation_count_impl X phi2 count)
-   | sp_bottom => 0
-   | sp_impl phi1 phi2 => (negation_count_impl X phi1 (count + 1)) + (negation_count_impl X phi2 count)
-   | sp_exists x phi' => negation_count_impl X phi' count
-(*    | sp_mX_dot_phi Y phi' => negation_count_impl X phi' count *)
-  end
-.
+  | sp_var x => true
+  | sp_set Y => if (svar_eq_dec Y X)
+                then (Nat.even nc)
+                else true
+  | sp_const sigma => true
+  | sp_app phi1 phi2 => andb (spos_accumulated phi1 X nc) (spos_accumulated phi2 X nc)
+  | sp_bottom => true
+  | sp_impl phi1 phi2 => andb (spos_accumulated phi1 X (S nc)) (spos_accumulated phi2 X nc)
+  | sp_exists x phi => spos_accumulated phi X nc
+  | sp_mu Y phi => if (svar_eq_dec Y X)
+                      then true
+                      else (spos_accumulated phi X nc)
+end.
 
-Fixpoint negation_count (X : SVar) (phi : Sigma_pattern) : nat := (negation_count_impl X phi 0).
-
-Definition is_positive (X : SVar) (phi : Sigma_pattern) := Nat.even (negation_count X phi).
-(* Notation "t is_positive_in X" := (is_positive X t) (at level 90, left associativity). *)
-
-(* -- felfele es lefele akkumulator valtozo bevezetese *)
+Fixpoint strictly_positive (phi : Sigma_pattern) (X : SVar) : bool :=  spos_accumulated phi X 0.
 
 Definition sp_eq_dec : forall (x y : Sigma_pattern), { x = y } + { x <> y }.
 Proof.
@@ -59,6 +57,7 @@ Proof.
   - exact (svar_eq_dec X X0).
   - exact (sigma_eq_dec sigma sigma0).
   - exact (evar_eq_dec x0 x1).
+  - exact (svar_eq_dec X X0).
 Defined.
 
 Definition sp_not (phi : Sigma_pattern) := sp_impl phi sp_bottom.
@@ -70,16 +69,44 @@ Definition sp_forall (x : EVar) (phi : Sigma_pattern) :=
   sp_not (sp_exists x (sp_not phi)).
 (*nuX.phi*)
 
-Fixpoint eSubstitution (phi : Sigma_pattern) (psi : Sigma_pattern) (x : EVar) := 
+Fixpoint eSubstitution (phi : Sigma_pattern) (psi : Sigma_pattern) (x : EVar) :=
   match phi with
-   | sp_var x' => if evar_eq_dec x x' then psi else sp_var x'
-   | sp_set X => sp_set X
-   | sp_const sigma => sp_const sigma
-   | sp_app phi1 phi2 => sp_app (eSubstitution phi1 psi x)
+  | sp_var x' => if evar_eq_dec x x' 
+                  then psi 
+                  else sp_var x'
+  | sp_set X => sp_set X
+  | sp_const sigma => sp_const sigma
+  | sp_app phi1 phi2 => sp_app (eSubstitution phi1 psi x)
                                 (eSubstitution phi2 psi x)
-   | sp_bottom => sp_bottom
-   | sp_impl phi1 phi2 => sp_impl (eSubstitution phi1 psi x)
+  | sp_bottom => sp_bottom
+  | sp_impl phi1 phi2 => sp_impl (eSubstitution phi1 psi x)
                                   (eSubstitution phi2 psi x)
-   | sp_exists x' phi' => sp_exists x' (eSubstitution phi' psi x)
-  end
-.
+  | sp_exists x' phi' => if (evar_eq_dec x' x) 
+                          then sp_exists x' phi'
+                          else sp_exists x' (eSubstitution phi' psi x)
+  | sp_mu X phi' => sp_mu X (eSubstitution phi' psi x)
+end.
+
+Definition var (name : string) : Sigma_pattern :=
+  sp_var (evar_c name).
+
+Definition set (name : string) : Sigma_pattern :=
+  sp_set (svar_c name).
+
+Definition simple_neg := (sp_not (set "X")).
+Eval compute in (strictly_positive simple_neg (svar_c "X") = false).
+
+Definition X_imp_X := (sp_impl (set "X") (set "X")).
+Eval compute in (strictly_positive X_imp_X (svar_c "X") = false).
+Eval compute in (strictly_positive (sp_not X_imp_X) (svar_c "X") = false).
+
+Definition X_i_X_i_X := (sp_impl (set "X") ((sp_impl (set "X") (set "X")))).
+
+Eval compute in (strictly_positive X_i_X_i_X (svar_c "X") = false).
+Eval compute in (strictly_positive (sp_not X_i_X_i_X)(svar_c "X") = false).
+
+Definition X_i_X_i_nX := (sp_impl (set "X") ((sp_impl (set "X") (sp_not (set "X"))))).
+
+Eval compute in (strictly_positive X_i_X_i_nX (svar_c "X") = false).
+Eval compute in (strictly_positive (sp_not X_i_X_i_nX) (svar_c "X") = true).
+
