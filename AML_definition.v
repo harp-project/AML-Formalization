aea27dd944ae45a1754fab5b3a872ffc51e60e76
Boros Attila Péter <attila9778@yahoo.com>
2020-03-08 23:22:39 +0100

    Natural numbers without induction v0.1

--- AML_definition.v
+++ AML_definition.v
@@ -70,6 +70,7 @@ Definition sp_not (phi : Sigma_pattern) := sp_impl phi sp_bottom.
 Definition sp_or  (l r : Sigma_pattern) := sp_impl (sp_not l) r.
 Definition sp_and (l r : Sigma_pattern) :=
   sp_not (sp_or (sp_not l) (sp_not r)).
+Notation "a _&_ b" := (sp_and a b) (left associativity, at level 40). (* TODO: Check associativity level *)
 Definition sp_tatu := sp_not sp_bottom.
 Definition sp_equiv (l r : Sigma_pattern) := (sp_and (sp_impl (l) (r)) (sp_impl (l) (r))).
 Definition sp_forall (x : EVar) (phi : Sigma_pattern) :=
@@ -319,7 +320,7 @@ Definition c_non_equality := sp_const(sigma_c("non-equality")).
 Definition NonEquality (l r : Sigma_pattern) := ((c_non_equality _._ l) _._ r).
 Notation "phi1 !=~ phi2" := (NonEquality phi1 phi2) (at level 100).
 
-(* Membership *)
+(* Variable membership *)
 Definition c_membership := sp_const(sigma_c("membership")).
 Definition Membership (x : EVar) (phi : Sigma_pattern) := 
   ((c_membership _._ (sp_var x)) _._ phi).
@@ -441,27 +442,160 @@ Notation "'[[' s ']]'" := (InhabitantSetOf s) (at level 100).
 
 (* f : s1 x s2 x ... x sn -> s *)
 
-Definition MSA_pair (M : set MSA_sorts) (Sigma : set (set Sigma_pattern)) := (M, Sigma).
-
-
-(* Natural numbers *)
-End MSA.
-
-
-(*
- - how to formalise equivalence between expressions? -> Definition, Notation
- - how to express n-length parameter list? ->
- - how can be interpreted "0: -> Nat" (section 3.1)? -> true
-   - an implication without parameter? then how can we formalise it?
-   - a function which gives back 0 as an element of sort Nat on every input parameter?
- - how to define the semantics of the other functions?
+(* Ezt nem lehet ugyanazert mint az Y-kombinatort *)
+(* Inductive D : Type :=
+| Single : set MSA_sorts
+| Multiple : (D, set MSA_sorts)
+.
  *)
 
-(* Inductive Nat : Type :=
-  | O    : Nat
-  | succ : Nat -> Nat
-  | plus : Nat x Nat -> Nat
-  | mult : Nat x Nat -> Nat
-. *)
+(* Definition MSA_signature (S : set MSA_sorts) := (S, (S*, S)). *)
+
+(* TODO:
+  how to emphasize constraints:
+  - M is non-empty
+  - Sigma depends on M
+*)
+(* Definition MSA_pair (M : set MSA_sorts) (Sigma : set (set Sigma_pattern)) := (M, Sigma). *)
+Reserved Notation "sp 'states'" (at level 1).
+
+Inductive Statement : Sigma_pattern -> Prop :=
+| S_pat {s : Sigma_pattern} : s states
+where "sp 'states'" := (Statement sp).
+
+Definition Nonempty_Sort (s : MSA_sorts) := ([[ s ]] !=~ sp_bottom) states.
+(* Proof.
+  intros.
+  eapply S_pat.
+Qed.
+ *)
+Require Import Coq.Lists.List.
+Definition sorts_list := cons (List) (cons (Cfg) nil).
+Check length sorts_list.
+Compute (length sorts_list).
+
+(* TODO: constructors of vector hide constructors of ListSet *)
+(* Require Import Coq.Vectors.VectorDef.
+Check of_list sorts_list.
+Check to_list (of_list sorts_list).
+
+Compute List.nth O (sorts_list) Nat.
+
+Definition v := of_list sorts_list.
+Compute (length sorts_list).
+Check v.
+
+Check tl v.
+
+Check to_list v.
+Check List.tl (to_list v).
+ *)
+
+(* TODO: for all below
+    - restrict n > 0
+    - rewrite to vector
+*)
+Fixpoint and_gen (n : nat) (vec : list Sigma_pattern) : Sigma_pattern :=
+match n - 1 with
+| O => (List.hd sp_bottom vec)
+| S n' => ((List.hd sp_bottom vec) _&_ (and_gen n' (List.tl vec)))
+end.
+
+Compute and_gen 2 (cons ( sp_var(evar_c("x1")) ) (cons ( sp_var(evar_c("x2")) ) nil)).
+
+(* Fixpoint associate_ (n : nat) (vars : list Sigma_pattern) (sorts : list MSA_sorts) : (list (Sigma_pattern * MSA_sorts)):=
+match n with
+| O => cons ((List.nth O vars sp_bottom), (List.nth O sorts_list Nat)) nil
+| S n' => cons
+      ((List.nth ((length vars) - n') vars sp_bottom), (List.nth ((length sorts) - n') sorts Nat))
+      (associate_ n' vars sorts)
+end. *)
+
+(* Definition x1 := sp_var(evar_c("x1")).
+Compute associate_ 1 (cons x1 nil) (cons Nat nil). *)
+
+Check evar_c("x").
+
+Fixpoint assoc_type_incl (n : nat) (vars : list EVar) (sorts : list MSA_sorts) : Sigma_pattern :=
+match n - 1 with
+| O => ((List.hd (evar_c("x")) vars) -< [[ (List.hd Nat sorts_list) ]])
+| S n' => sp_and
+      ( (List.hd (evar_c("x")) vars) -< [[ (List.hd Nat sorts) ]] )
+      (assoc_type_incl n' (List.tl vars) (List.tl sorts))
+end.
+
+Definition var_x1 := evar_c("x1").
+Compute assoc_type_incl 1 (cons var_x1 nil) (cons Nat nil).
+
+Fixpoint assoc_fun_with_params (f : Sigma) (n : nat) (vars : list EVar) : Sigma_pattern :=
+match n - 1 with
+| O => ((sp_const f) _._ (sp_var (List.nth O vars (evar_c("x")))))
+| S n' => (assoc_fun_with_params f n' vars) _._ (sp_var (List.nth (S n') vars (evar_c("x"))))
+end.
+
+Definition fun_f := sigma_c("f").
+Compute assoc_fun_with_params fun_f 1 (cons var_x1 nil).
+Definition var_x2 := evar_c("x2").
+Compute assoc_fun_with_params fun_f 2 (cons var_x1 (cons var_x2 nil)).
+Check assoc_fun_with_params fun_f 2 (cons var_x1 (cons var_x2 nil)).
+
+Definition var_y := evar_c("y").
+Check (assoc_fun_with_params fun_f 2 (cons var_x1 (cons var_x2 nil))) ~=~ (sp_var var_y).
+
+Definition Function (n : nat) (vars : list EVar) (sorts : list MSA_sorts) (f : Sigma) (y : EVar) (s : MSA_sorts) :=
+(sp_impl
+  (* (sp_and sp_and (x1 -< [[ s1 ]]) (x2 -< [[ s2 ]])) .. (xn -< [[ sn ]])) *)
+  (assoc_type_incl n vars sorts)
+
+  (sp_exists y (
+    sp_and
+      (y -< [[ s ]])
+
+      (* (sp_app (sp_app f x1) .. xn) ~=~ y *)
+      ((assoc_fun_with_params f n vars) ~=~ (sp_var y))
+    )
+  )
+).
+(* Proof.
+  intros.
+  eapply S_pat.
+Qed. *)
+
+(* This will be possible only when vector will be used, because of length coercion *)
+(* (* f : s1 x s2 x ... x sn -> s *)
+Notation "f : s1 x s2 x ... x sn -> s" := Function ... . *)
+
+(* TODO: Sort membership *)
+Definition c_sort := sp_const(sigma_c("Sort")). (* represents the sort set, containing all sorts *)
+(* Definition Sort (s : MSA_sorts) := s  c_sort. *)
+
+End MSA.
+
+(* Natural numbers *)
+Section NaturalNumbers.
+
+Definition zero (y : EVar) := (Function 1 nil nil (sigma_c("0")) y Nat).
+
+Definition succ (x0 y : EVar) := (Function 1 (cons x0 nil) (cons Nat nil) (sigma_c("succ")) y Nat).
+
+Definition plus (x0 x1 y : EVar) := (Function 1 (cons x0 (cons x1 nil)) (cons Nat (cons Nat nil)) (sigma_c("plus")) y Nat).
+
+Definition mult (x0 x1 y : EVar) := (Function 1 (cons x0 (cons x1 nil)) (cons Nat (cons Nat nil)) (sigma_c("mult")) y Nat).
+
+(* TODO: generalize all definitions to Sigma_pattern, and restrict these to EVar and function (e.g. 0) *)
+
+(* Example: x + 0 = x *)
+Definition x := (evar_c("x")).
+Definition y := (evar_c("y")).
+Definition const_0 := (evar_c("0")).
+Definition x_plus_0_eq_x := sp_forall x (
+  sp_impl
+    (x -< InhabitantSetOf(Nat) )
+    ((plus x const_0 y) ~=~ (sp_var x))
+).
+
+(* TODO: introduce the notation of ':' *)
+
+End NaturalNumbers.
 
 End AML.
