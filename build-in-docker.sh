83d8d551dffdc789294792014ca9b1ef6c94f67f
Jan Tusil <jenda.tusil@gmail.com>
2020-07-31 12:27:40 +0200

    test

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -3,6 +3,6 @@
 docker build -t ml-in-coq .
 ./run-in-docker.sh whoami
 ./run-in-docker.sh id -u
-./run-in-docker.sh ls -l
+./run-in-docker.sh ls -la
 ./run-in-docker.sh coq_makefile -f _CoqProject -o Makefile
 ./run-in-docker.sh make
