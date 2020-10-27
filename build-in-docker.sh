8db6a35301f8ee95b048b51f2bd983ba4e5415ba
Jan Tusil <jenda.tusil@gmail.com>
2020-10-27 13:34:07 +0100

    CI: chown -R

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -2,8 +2,6 @@
 
 docker build -t ml-in-coq .
 # In Travis-CI, the user has different id than in the container.
-./run-in-docker.sh sudo chown coq:coq .
+./run-in-docker.sh sudo chown -R coq:coq .
 ./run-in-docker.sh coq_makefile -f _CoqProject -o Makefile
-./run-in-docker.sh ls -laZ
-./run-in-docker.sh ls -laZ ./ML
 ./run-in-docker.sh make
