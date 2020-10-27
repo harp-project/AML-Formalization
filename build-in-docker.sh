97b9c778a9beb624b34b95d88f44ad9fd998d547
Jan Tusil <jenda.tusil@gmail.com>
2020-10-27 13:24:48 +0100

    CI debugging info

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -4,4 +4,6 @@ docker build -t ml-in-coq .
 # In Travis-CI, the user has different id than in the container.
 ./run-in-docker.sh sudo chown coq:coq .
 ./run-in-docker.sh coq_makefile -f _CoqProject -o Makefile
+./run-in-docker.sh ls -laZ
+./run-in-docker.sh ls -laZ ./ML
 ./run-in-docker.sh make
