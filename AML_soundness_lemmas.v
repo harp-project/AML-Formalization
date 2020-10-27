5fbb398d6334059038a0f372322faa6fddcfce1e
Jan Tusil <jenda.tusil@gmail.com>
2020-10-27 17:59:23 +0100

    make AML_soundness_lemmas build

--- AML_soundness_lemmas.v
+++ AML_soundness_lemmas.v
@@ -1,28 +1,31 @@
 Require Import Arith.
 Require Import ZArith.
 Require Import List.
+Require Import Coq.micromega.Lia.
+Require Export String.
+Require Export Coq.Lists.ListSet.
+Require Export Coq.Program.Wf.
+
 Require Import extralibrary.
-Require Import names.
+(*Require Import names.*)
+Require Import ML.Signature.
 Require Export locally_nameless.
 Require Import Lattice.
 
-Require Import Coq.micromega.Lia.
-
-Require Export String.
-Require Export Coq.Lists.ListSet.
 Require Export Ensembles_Ext.
 
-Require Export Coq.Program.Wf.
 
-Check In.
-Check @ext_valuation.
+Import MLNotations.
 
+Section soundness_lemmas.
+
+  Context {sig : Signature}.
 
 Definition respects_blacklist (phi : Pattern) (Bp Bn : Ensemble svar_name) : Prop :=
   forall (var : svar_name),
-    (Bp var -> negative_occurrence var phi) /\ (Bn var -> positive_occurrence var phi).
+    (Bp var -> negative_occurrence var phi) /\ (Bn var -> @positive_occurrence sig var phi).
 
-Definition ModelPowersetLattice (M : Model) := PowersetLattice (Domain M).
+Definition ModelPowersetLattice (M : Model) := PowersetLattice (@Domain sig M).
 
 
 Lemma respects_blacklist_implies_monotonic :
@@ -36,12 +39,12 @@ Lemma respects_blacklist_implies_monotonic :
       Included (Domain M) S1 S2 ->
       (Bp X ->
        Included (Domain M)
-                (@ext_valuation M evar_val (update_valuation svar_name_eqb X S2 svar_val) phi)
-                (@ext_valuation M evar_val (update_valuation svar_name_eqb X S1 svar_val) phi)
+                (@ext_valuation sig M evar_val (update_valuation svar_name_eqb X S2 svar_val) phi)
+                (@ext_valuation sig M evar_val (update_valuation svar_name_eqb X S1 svar_val) phi)
       ) /\ (Bn X ->
             Included (Domain M)
-                     (@ext_valuation M evar_val (update_valuation svar_name_eqb X S1 svar_val) phi)
-                     (@ext_valuation M evar_val (update_valuation svar_name_eqb X S2 svar_val) phi)
+                     (@ext_valuation sig M evar_val (update_valuation svar_name_eqb X S1 svar_val) phi)
+                     (@ext_valuation sig M evar_val (update_valuation svar_name_eqb X S2 svar_val) phi)
          )
 .
 Proof.
@@ -71,7 +74,6 @@ Lemma is_monotonic :
 Proof.
 Admitted.
 *)
-
 (* if ext_valuation (phi1 --> phi2) = Full_set 
    then ext_valuation phi1 subset ext_valuation phi2
 *)
@@ -80,7 +82,7 @@ Theorem ext_valuation_implies_subset {m : Model}
         (phi1 : Pattern) (phi2 : Pattern) :
   Same_set (Domain m) (ext_valuation evar_val svar_val (phi1 --> phi2)) (Full_set (Domain m)) ->
   Included (Domain m) (ext_valuation evar_val svar_val phi1)
-           (ext_valuation evar_val svar_val phi2).
+           (@ext_valuation sig m evar_val svar_val phi2).
 Proof.
   intros; unfold Included; intros.
   rewrite ext_valuation_imp_simpl in H.
@@ -93,11 +95,11 @@ Qed.
 
 (* evar_open of fresh name does not change *)
 Lemma evar_open_fresh (phi : Pattern) :
-  forall n, well_formed phi -> evar_open n fresh_evar_name phi = phi.
-Proof.
+  forall n, well_formed phi -> evar_open n (evar_fresh (variables sig) (free_evars phi)) phi = phi.
+Proof. Admitted. (*
   intros. generalize dependent n. induction phi; intros; simpl; try eauto.
   * inversion H. inversion H1.
-  * rewrite IHphi1. rewrite IHphi2. reflexivity.
+  *  rewrite IHphi1. rewrite IHphi2. reflexivity.
     split; inversion H. inversion H0; assumption. inversion H1; assumption.
     split; inversion H. inversion H0; assumption. inversion H1; assumption.
   * rewrite IHphi1. rewrite IHphi2. reflexivity.
@@ -106,13 +108,16 @@ Proof.
   * rewrite IHphi. reflexivity.
     split; inversion H. assumption.
     unfold well_formed_closed in *. simpl in H1. admit.
-Admitted.
+Admitted.*)
 
 (* update_valuation with fresh name does not change *)
+(* TODO(jan.tusil): I think that we need to generalize this
+   to work with any variable that is not free in psi.
+*)
 Lemma update_valuation_fresh {m : Model}
       (evar_val : evar_name -> Domain m) (svar_val : svar_name -> Power (Domain m))
       (psi : Pattern) (x : Domain m) (c : Domain m) :
-  ext_valuation (update_valuation evar_name_eqb fresh_evar_name c evar_val) svar_val psi x
+  ext_valuation (update_valuation evar_name_eqb (evar_fresh (variables sig) (free_evars psi)) c evar_val) svar_val psi x
   = ext_valuation evar_val svar_val psi x.
 Proof.
 Admitted.
@@ -122,6 +127,8 @@ Lemma update_valuation_free_svar_subst {m : Model}
       (evar_val : evar_name -> Domain m) (svar_val : svar_name -> Power (Domain m))
       (phi : Pattern) (psi : Pattern) (X : svar_name) :
   ext_valuation evar_val svar_val phi
-  = ext_valuation evar_val svar_val (free_svar_subst phi psi X) .
+  = ext_valuation evar_val svar_val (@free_svar_subst sig phi psi X) .
 Proof.
 Admitted.
+
+End soundness_lemmas.
