f3cac197a4bf83cd070b333cf830d5af5b365cdd
Jan Tusil <jenda.tusil@gmail.com>
2020-12-28 13:21:38 +0100

    pattern_interpretation_exists_of_sort_predicate

--- src/Semantics.v
+++ src/Semantics.v
@@ -974,6 +974,24 @@ repeat
         rewrite <- Same_set_Compl.
         assumption.
     Qed.
+
+    Lemma pattern_interpretation_and_full M ρₑ ρₛ ϕ₁ ϕ₂:
+      @pattern_interpretation M ρₑ ρₛ (patt_and ϕ₁ ϕ₂) = Full
+      <-> (@pattern_interpretation M ρₑ ρₛ ϕ₁ = Full
+           /\ @pattern_interpretation M ρₑ ρₛ ϕ₂ = Full).
+    Proof.
+      unfold Full.
+      rewrite -> pattern_interpretation_and_simpl.
+      split.
+      - intros H.
+        apply Intersection_eq_Full_eq in H.
+        auto.
+      - intros [H1 H2].
+        rewrite -> H1. rewrite -> H2.
+        apply Same_set_to_eq.
+        apply Intersection_same.
+    Qed.
+      
     
     (* Theory,axiom ref. snapshot: Definition 5 *)
 
--- src/Theories/Sorts.v
+++ src/Theories/Sorts.v
@@ -143,6 +143,94 @@ Section sorts.
         }
         apply Hfeip2.
     Qed.
+
+    Lemma pattern_interpretation_exists_of_sort_predicate s ϕ ρₑ ρₛ:
+      (* s is closed *)
+      free_evars s = base.empty ->
+      well_formed_closed s ->
+      let x := fresh_evar ϕ in
+      M_predicate M (evar_open 0 x ϕ) ->
+      pattern_interpretation ρₑ ρₛ (patt_exists_of_sort s ϕ) = Full
+      <-> (∃ m : Domain M, Minterp_inhabitant s ρₑ ρₛ m /\
+                           pattern_interpretation (update_evar_val x m ρₑ) ρₛ (evar_open 0 x ϕ) = Full).
+    Proof.
+      intros Hs Hwfc x Hpred.
+      unfold patt_exists_of_sort.
+      assert (Hfr: fresh_evar (patt_in b0 (inhabitant_set s) and ϕ) = fresh_evar ϕ).
+      { unfold fresh_evar. apply f_equal. apply f_equal. simpl.
+        rewrite -> Hs.
+        repeat rewrite -> sets.union_empty_r_L.
+        rewrite -> sets.union_empty_l_L.
+        auto.
+      }
+      rewrite -> pattern_interpretation_exists_predicate_full.
+      2: {
+        autorewrite with ml_db.
+        simpl.
+        apply M_predicate_and.
+        - apply T_predicate_in.
+          apply M_satisfies_theory.
+        - subst x.
+          rewrite -> Hfr.
+          apply Hpred.
+      }
+      subst x.
+      split.
+      - intros [m H].
+        autorewrite with ml_db in H. simpl in H.
+        rewrite -> Hfr in H.
+        apply pattern_interpretation_and_full in H.
+        destruct H as [H1 H2].
+        exists m. split. 2: apply H2. clear H2.
+        unfold Minterp_inhabitant.
+        pose proof (Hfeip := free_evar_in_patt M M_satisfies_theory (fresh_evar ϕ) (patt_sym (inj inhabitant) $ evar_open 0 (fresh_evar ϕ) s) (update_evar_val (fresh_evar ϕ) m ρₑ) ρₛ).
+        destruct Hfeip as [_ Hfeip2]. apply Hfeip2 in H1. clear Hfeip2.
+        rewrite update_evar_val_same in H1.
+        unfold sym.
+        unfold Ensembles.In in H1.
+        rewrite -> evar_open_wfc in H1. 2: apply Hwfc.
+        rewrite -> pattern_interpretation_free_evar_independent in H1.
+        3: { intros Contra. simpl in Contra.
+             rewrite -> sets.union_empty_l_L in Contra.
+             rewrite -> Hs in Contra.
+             apply base.not_elem_of_empty in Contra.
+             apply Contra.
+        }
+        2: { apply wfc_ind_wfc.
+             constructor.
+             constructor.
+             apply wfc_wfc_ind.
+             apply Hwfc.
+        }
+        apply H1.
+      - intros [m [H1 H2]]. exists m.
+        autorewrite with ml_db. simpl.
+        pose proof (Hfeip := free_evar_in_patt M M_satisfies_theory (fresh_evar ϕ) (patt_sym (inj inhabitant) $ evar_open 0 (fresh_evar ϕ) s) (update_evar_val (fresh_evar ϕ) m ρₑ) ρₛ).
+        destruct Hfeip as [Hfeip1 _].
+        apply pattern_interpretation_and_full.
+        rewrite -> Hfr.
+        split.
+        + apply Hfeip1. clear Hfeip1.
+          unfold Ensembles.In.
+          rewrite -> update_evar_val_same.
+          unfold Minterp_inhabitant in H1. unfold sym in H1.
+          rewrite -> evar_open_wfc. 2: apply Hwfc.
+          rewrite -> pattern_interpretation_free_evar_independent.
+          3: { intros Contra. simpl in Contra.
+               rewrite -> sets.union_empty_l_L in Contra.
+               rewrite -> Hs in Contra.
+               apply base.not_elem_of_empty in Contra.
+               apply Contra.
+          }
+          2: { apply wfc_ind_wfc.
+               constructor.
+               constructor.
+               apply wfc_wfc_ind.
+               apply Hwfc.
+          }
+          apply H1.
+        + apply H2.
+    Qed.
     
     Lemma interp_total_function f s₁ s₂ ρₑ ρₛ :
       @pattern_interpretation sig M ρₑ ρₛ (patt_total_function f s₁ s₂) = Full ->
