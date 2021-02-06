1c6017ab24fa95a4c16d684723a93fafadb30401
Jan Tusil <jenda.tusil@gmail.com>
2021-02-06 13:56:04 +0100

    WIP

--- default.nix
+++ default.nix
@@ -16,6 +16,7 @@ let
         src = fetchGit {
           url = "https://gitlab.mpi-sws.org/iris/stdpp.git";
           rev = "bcebd707d223fe539f8db924dc3bac1fe8c6e678";
+          /* rev = lib.strings.fileContents "./deps/stdpp.rev"; */
         };
         buildInputs = with coq.ocamlPackages; [ ocaml camlp5 ];
         propagatedBuildInputs = [ coq ];
@@ -23,6 +24,7 @@ let
 
         installFlags = [ "COQLIB=$(out)/lib/coq/${coq.coq-version}/" ];
       } ) { } ;
+/*  let deps = import "nix/deps.nix" { coqVersion };*/
   self = stdenv.mkDerivation {
     name = "coq${ncoq.coq-version}-matching-logic";
 
