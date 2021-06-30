ed0f57ba6041e54f849a92c7e601c892b46bd961
Jan Tusil <jenda.tusil@gmail.com>
2021-06-30 13:28:34 +0200

    An attempt to build with mtac2

--- nix/deps.nix
+++ nix/deps.nix
@@ -24,4 +24,64 @@ let
 
         installFlags = [ "COQLIB=$(out)/lib/coq/${coq.coq-version}/" ];
       } ) { } ;
-in { inherit stdpp; coq = ncoq;  }
+
+  unicoq = ncoqPackages.callPackage
+    ( { coq, stdenv, fetchFromGithub }:
+      stdenv.mkDerivation {
+        name = "coq${coq.coq-version}-unicoq";
+
+        src = fetchGit {
+          url = "https://github.com/unicoq/unicoq.git";
+          ref = "master-8.13";
+          rev = lib.strings.fileContents ../deps/unicoq.rev;
+        };
+
+        patches = [../deps/unicoq-meta.patch];
+
+        configurePhase = "coq_makefile -f _CoqProject -o Makefile";
+
+        buildInputs = with coq.ocamlPackages; [ ocaml camlp5 zarith];
+        propagatedBuildInputs = [ coq coq.ocamlPackages.findlib ];
+        enableParallelBuilding = true;
+
+        installFlags = [ "COQLIB=$(out)/lib/coq/${coq.coq-version}/" ];
+        installPhase = ''
+          make "COQLIB=$out/lib/coq/${coq.coq-version}" install
+          cp ocaml/META "$out/lib/coq/${coq.coq-version}/user-contrib/Unicoq"
+        '';
+
+        setupHook = writeText "setupHook.sh" ''
+          export OCAMLPATH="''${OCAMLPATH:-""}''${OCAMLPATH:+:}''$1/lib/coq/${coq.coq-version}/user-contrib/"
+        '';
+
+
+      } ) { } ;
+
+  mtac2 = ncoqPackages.callPackage
+    ( { coq, stdenv, fetchFromGithub }:
+      stdenv.mkDerivation {
+        name = "coq${coq.coq-version}-mtac2";
+
+        src = fetchGit {
+          url = "https://github.com/Mtac2/Mtac2.git";
+          rev = lib.strings.fileContents ../deps/mtac2.rev;
+        };
+
+        postPatch = ''
+          patchShebangs --build configure.sh
+        '';
+
+
+        configurePhase = "mkdir -p .git/hooks; ./configure.sh";
+        buildPhase = ''
+          make VERBOSE=1 CAMLPKGS='-package Unicoq'
+        '';
+
+        buildInputs = with coq.ocamlPackages; [ ocaml camlp5 unicoq ];
+        propagatedBuildInputs = [ coq unicoq ];
+        enableParallelBuilding = true;
+
+        installFlags = [ "COQLIB=$(out)/lib/coq/${coq.coq-version}/" ];
+      } ) { } ;
+
+in { inherit stdpp; inherit mtac2; coq = ncoq;  }
