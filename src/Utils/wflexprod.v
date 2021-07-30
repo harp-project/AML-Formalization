1dafd74a3b29041463a53c0d57ef2705c33e3dcc
Jan Tusil <jenda.tusil@gmail.com>
2021-07-30 09:47:13 +0200

    well-founded lexicographic ordering

---
+++ src/Utils/wflexprod.v
@@ -0,0 +1,33 @@
+(* Taken from https://github.com/proofengineering/regmatch/blob/qed-at-large/regmatch.v#L67 *)
+From Coq Require Import ssreflect ssrfun ssrbool.
+
+Section lexprod'.
+
+Variable A : Type.
+
+Variable ltA : A -> A -> Prop.
+
+Inductive lexprod' : A * A -> A * A -> Prop :=
+| left_lex' : forall (x1 x2 y1 y2 : A), ltA x1 x2 -> lexprod' (x1, y1) (x2, y2)
+| right_lex' : forall (x y1 y2 : A), ltA y1 y2 -> lexprod' (x, y1) (x, y2).
+
+Lemma lexprod'_Acc : well_founded ltA -> forall x, Acc ltA x -> forall y, Acc ltA y -> Acc lexprod' (x, y).
+Proof.
+intros H x Hx.
+induction Hx as [x _ IHacc].
+intros y Hy.
+induction Hy as [y _ IHacc0].
+apply Acc_intro.
+intros (x1, y1) HA.
+inversion HA; subst.
+- apply IHacc; auto.
+- apply IHacc0; auto.
+Defined.
+
+Theorem wf_lexprod' : well_founded ltA -> well_founded lexprod'.
+Proof.
+intros H_wf (x, y).
+by auto using lexprod'_Acc.
+Defined.
+
+End lexprod'.
