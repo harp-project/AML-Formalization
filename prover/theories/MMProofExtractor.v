53640cb5d233c47ede838176a51515189ddf7589
Jan Tusil <jenda.tusil@gmail.com>
2021-08-10 15:42:48 +0200

    plugin wip

--- prover/theories/MMProofExtractor.v
+++ prover/theories/MMProofExtractor.v
@@ -1,6 +1,8 @@
 From MatchingLogic Require Import Syntax ProofSystem.
 From MatchingLogicProver Require Import MMProofExtractorLoader.
 
+Write MetaMath Proof Object File "myfile.mm".
+
 HelloWorld.
 
 Lemma test : True.
