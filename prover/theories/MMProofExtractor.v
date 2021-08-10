8d51b3e9a13a2cc0515cb348567688a2031f30a1
Jan Tusil <jenda.tusil@gmail.com>
2021-08-10 15:02:43 +0200

    a hello-world plugin

---
+++ prover/theories/MMProofExtractor.v
@@ -0,0 +1,10 @@
+From MatchingLogic Require Import Syntax ProofSystem.
+From MatchingLogicProver Require Import MMProofExtractorLoader.
+
+HelloWorld.
+
+Lemma test : True.
+Proof.
+hello_world.
+Abort.
+HelloWorld.
