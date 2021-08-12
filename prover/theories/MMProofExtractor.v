2a15d8f2f96acc774004661f906df0c0a1b15868
Jan Tusil <jenda.tusil@gmail.com>
2021-08-12 14:54:42 +0200

    WIP toString

--- prover/theories/MMProofExtractor.v
+++ prover/theories/MMProofExtractor.v
@@ -1,4 +1,4 @@
-From Coq Require Import String.
+From Coq Require Import Strings.String.
 From stdpp Require Import base finite.
 From MatchingLogic Require Import Syntax ProofSystem.
 From MatchingLogicProver Require Import MMProofExtractorLoader.
@@ -17,18 +17,20 @@ Module MetaMath.
   Inductive DisjointStmt := ds (lv : list Variabl).
   Inductive TypeCode := tc (c : Constant).
 
-  Inductive FloatingStmt := fs (tc : TypeCode) (var : Variabl).
-  Inductive EssentialStmt := es (tc : TypeCode) (lms : list MathSymbol).
+  
+  Inductive Label := lbl (s : string).
+  
+  Inductive FloatingStmt := fs (l : Label) (tc : TypeCode) (var : Variabl).
+  Inductive EssentialStmt := es (l : Label) (tc : TypeCode) (lms : list MathSymbol).
   
   
   Inductive HypothesisStmt :=
   | hs_floating (fs : FloatingStmt)
-  | hs_esential (es : EssentialStmt)
+  | hs_essential (es : EssentialStmt)
   .
 
-  Inductive AxiomStmt := axs (tc : TypeCode) (lms : list MathSymbol).
+  Inductive AxiomStmt := axs (l : Label) (tc : TypeCode) (lms : list MathSymbol).
 
-  Inductive Label := lbl (s : string).
   Inductive MMProof := pf (ll : list Label).
   
   Inductive ProvableStmt := ps (tc : TypeCode) (lms : list MathSymbol) (pf : MMProof).
@@ -53,6 +55,139 @@ Module MetaMath.
   
   Definition Database := list OutermostScopeStmt.
 
+  (* Concrete syntax printing. *)
+  
+  Definition IncludeStmt_toString (x : IncludeStmt) :=
+    match x with
+    | include_stmt s => append "$[ " (append s " $]")
+    end.
+  
+
+  Definition MathSymbol_toString (x : MathSymbol) :=
+    match x with
+    | ms s => s
+    end.
+  
+  Definition Constant_toString (x : Constant) :=
+    match x with
+    | constant s => MathSymbol_toString s
+    end.
+
+  Definition ConstantStmt_toString (x : ConstantStmt) : string :=
+    match x with
+    | constant_stmt cs => append "$c" (append (foldr append " "%string (map Constant_toString cs)) "$.")
+    end.
+  
+  Definition Variabl_toString (x : Variabl) :=
+    match x with
+    | variable s => s
+    end.
+  
+  Definition VariableStmt_toString (x : VariableStmt) : string :=
+    match x with
+    | vs lv => append "$v" (append (foldr append " "%string (map Variabl_toString lv)) "$.")
+    end.
+  
+
+  Definition DisjointStmt_toString (x : DisjointStmt) : string :=
+    match x with
+    | ds lv => append "$d" (append (foldr append " "%string (map Variabl_toString lv)) "$.")
+    end.
+  
+  Definition TypeCode_toString (x : TypeCode) : string :=
+    match x with
+    | tc c => Constant_toString c
+    end.
+    
+  Definition Label_toString (x : Label) : string :=
+    match x with
+    | lbl s => s
+    end.
+  
+  
+  Definition FloatingStmt_toString (x : FloatingStmt) : string :=
+    match x with
+    | fs l t var => append
+                      (Label_toString l)
+                      (append
+                         "$f"
+                         (append
+                            (append (TypeCode_toString t) (Variabl_toString var))
+                            "$."
+                         )
+                      )
+    end.
+
+    Definition EssentialStmt_toString (x : EssentialStmt) : string :=
+    match x with
+    | es l t lms => append
+                      (Label_toString l)
+                      (append
+                         "$e"
+                         (append
+                            (append
+                               (TypeCode_toString t)
+                               (foldr append " "%string (map MathSymbol_toString lms))
+                            )
+                            "$."
+                         )
+                      )
+    end.
+
+    Definition HypothesisStmt_toString (x : HypothesisStmt) : string :=
+      match x with
+      | hs_floating f => FloatingStmt_toString f
+      | hs_essential e => EssentialStmt_toString e
+      end.
+    
+
+    
+    Definition AxiomStmt_toString (x : AxiomStmt) : string :=
+    match x with
+    | axs l t lms => append
+                      (Label_toString l)
+                      (append
+                         "$a"
+                         (append
+                            (append
+                               (TypeCode_toString t)
+                               (foldr append " "%string (map MathSymbol_toString lms))
+                            )
+                            "$."
+                         )
+                      )
+    end.
+
+    Definition MMProof_toString (x : MMProof) : string :=
+      match x with
+      | pf ll =>  foldr append " "%string (map Label_toString ll)
+      end.
+
+    
+    
+  Inductive ProvableStmt := ps (tc : TypeCode) (lms : list MathSymbol) (pf : MMProof).
+  
+  Inductive AssertStmt :=
+  | as_axiom (axs : AxiomStmt)
+  | as_provable (ps : ProvableStmt)
+  .
+  
+  Inductive Stmt :=
+  | stmt_block (ls : list Stmt)
+  | stmt_variable_stmt (vs : VariableStmt)
+  | stmt_disj_stmt (ds : DisjointStmt)
+  | stmt_hyp_stmt (hs : HypothesisStmt)
+  | stmt_assert_stmt (ass : AssertStmt).
+  
+  Inductive OutermostScopeStmt :=
+  | oss_inc (incs : IncludeStmt)
+  | oss_cs (cs : ConstantStmt)
+  | oss_s (st : Stmt)
+  .
+  
+  Definition Database := list OutermostScopeStmt.
+
+
 End MetaMath.
 
 Import MetaMath.
@@ -63,18 +198,53 @@ Section gen.
     (symbolPrinter : symbols -> string)
   .
 
-  Definition axiomForSymbol (s : symbols) : MetaMath.OutermostScopeStmt :=
+  Definition constantForSymbol (s : symbols) : OutermostScopeStmt :=
+    oss_cs (constant_stmt [constant (ms (symbolPrinter s))]).
+  
+  Definition axiomForSymbol (s : symbols) : OutermostScopeStmt :=
     oss_s (stmt_assert_stmt (as_axiom (axs
                                          (tc (constant (ms (symbolPrinter s ++ "-is-pattern"))))
                                          [(ms "#Pattern"); (ms (symbolPrinter s))]
           ))).
 
+  Definition constantAndAxiomForSymbol (s : symbols) : Database :=
+    [constantForSymbol s; axiomForSymbol s].
+  
   Definition generateSymbolAxioms : Database :=
     map (axiomForSymbol) (@enum symbols (@sym_eq signature) finiteSymbols).
- 
+ '
 End gen.
+
+
+Module MMTest.
+
+  Require Import MatchingLogic.SignatureHelper.
+  Import MetaMath.
+
+  Inductive Symbol := a | b | c .
+
+
+  Instance Symbol_eqdec : EqDecision Symbol.
+  Proof.
+    intros s1 s2. unfold Decision. decide equality.
+  Defined.
+
+  Instance Symbol_h : SymbolsH Symbol := Build_SymbolsH Symbol Symbol_eqdec.
+  
+  Instance signature : Signature := @SignatureFromSymbols Symbol _.
+
+  Definition symbolPrinter (s : Symbol) : string :=
+    match s with
+    | a => "a"
+    | b => "b"
+    | c => "c"
+    end.
+  
+  
+End MMTest.
+
+
 
 Definition myMetamathProofObject : string := "Hi" ++ " " ++ "proof".
 
 Write MetaMath Proof Object File "myfile.mm" myMetamathProofObject.
-
