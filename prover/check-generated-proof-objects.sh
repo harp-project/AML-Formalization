68839869d7bed5492182dc72e0edb0a6222c9421
Jan Tusil <jenda.tusil@gmail.com>
2021-08-15 18:03:17 +0200

    `make test` now checks

--- prover/check-generated-proof-objects.sh
+++ prover/check-generated-proof-objects.sh
@@ -2,14 +2,28 @@
 
 compare() {
   NAME="$1"
-  GENERATED="$NAME.mm"
-  REFERENCE="ref/$NAME.mm.ref"
+  GENERATED="$NAME"
+  REFERENCE="ref/$NAME.ref"
 
   cat "$GENERATED" | grep the-proof | sed 's/the-proof $p |- \(.*\) $=.*/\1/' > $NAME.tmp
   diff -uZ "$NAME.tmp" "$REFERENCE"
 }
 
-mm_files = $(ls *.mm)
-echo $mm_files"
+RESULT="0"
+mm_files=$(ls *.mm)
+for mm_file in $mm_files
+do
+    echo "Checking $mm_file"
+    compare "$mm_file"
+    if [[ "$?" -gt 0 ]]
+    then
+	echo "ERROR"
+	RESULT="1"
+	continue
+    fi
+    
+    echo "verify proof *" | metamath "$mm_file"
+done
 
-compare proof_4
+exit "$RESULT"
+# compare proof_4.mm
