1608a64d44d7328b8d89f9a5bba476f0133dffb3
Jan Tusil <jenda.tusil@gmail.com>
2021-09-05 17:41:28 +0200

    Add FOL embedding to the project (2) (#163)
    * Add FOL embedding to the project
    
    * Build LN_FOL.v
    
    * Build Example 03 in CI
    
    * forgotten nix directory
    
    Co-authored-by: berpeti <peti.ber007@gmail.com>

---
+++ examples/03_FOL/shell.nix
@@ -0,0 +1,19 @@
+let
+  sources = import ../../nix/sources.nix;
+  pinned = import sources."nixpkgs" { config = {}; overlays = []; };
+in
+
+{ coqVersion ? "8.13"}:
+with import <nixpkgs> {};
+
+let
+
+  deps = import ./nix/deps.nix { inherit coqVersion; };
+  
+  self = mkShell {
+    name="matching-logic-example-03-FOL";
+    buildInputs = [deps.coq deps.mllib]; 
+  };
+
+in
+  self
