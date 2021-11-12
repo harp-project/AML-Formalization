b8fafb02dc888ce7f60fbd0eb3cb3fc22f5826c4
Vadász András <csagataj2@gmail.com>
2021-11-12 09:49:13 +0100

    Add script to fix unicode chars
    Saving a file in CoqIDE with nix replaces
    unicode characters with their code.
    https://github.com/coq/coq/issues/11526
    The python script reverts these changes.
    A small lemma is added.

---
+++ scripts/convert_to_unicode.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import re
+
+def to_unicode(s: str):
+	return bytes(s,'ascii').decode("unicode-escape")
+
+def find_unicodes(f: Path):
+	try:
+		patt = re.compile(r'\\'+'u[a-f0-9]{4}')
+		return f, set(patt.findall(f.open('r').read()))
+	except UnicodeDecodeError:
+		raise
+if __name__ == '__main__':
+	AML = Path.cwd()
+	while AML.name != 'AML-Formalization':
+		AML = AML.parent
+	root = AML / 'matching-logic' / 'src'
+	replaces = []
+	print('The following unicodes were found in the following .v files:')
+	for f in root.glob('*.v'):
+		res = find_unicodes(f)
+		if len(res[1]) > 0:
+			print(res[0].relative_to(AML))
+			for i in res[1]:
+				replaces.append((i,to_unicode(i)))
+				print(f'\t{replaces[-1]}')
+	print(f"{len(replaces)} codes were replaced in the .v files")
+	replaces = set(replaces)
+	for f in root.glob('*.v'):
+		data = f.open('r').read()
+		for esc, uni in replaces:
+			data = data.replace(esc,uni)
+		f.open("w",encoding='utf8').write(data)
