0155d7e2cc9a70f603d99820c11c2d43e099bc80
Jan Tusil <jenda.tusil@gmail.com>
2022-07-10 21:29:51 +0200

    matching-logic-doc

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
         ( { coq, stdenv }:
         stdenv.mkDerivation {
           name = "coq-matching-logic-doc";
-          src = self + "/matching-logic";
+          src = self + "/matching-logic-doc";
           buildInputs = [
             self.outputs.packages.${system}.coq-matching-logic
             pkgs.python310Packages.alectryon
@@ -42,7 +42,7 @@
           ];
           
           buildPhase = ''
-            make coqdoc "PATH_TO_COMPILED_LIBRARY=${self.outputs.packages.${system}.coq-matching-logic}/lib/coq/${coq.coq-version}/MatchingLogic}"
+            make coqdoc "PATH_TO_COMPILED_LIBRARY=${self.outputs.packages.${system}.coq-matching-logic}/lib/coq/${coq.coq-version}/MatchingLogic"
           '';
 
           installTargets = "install-coqdoc";
