2d311d8ad1033d97385738f79881232386804b0f
Jan Tusil <jenda.tusil@gmail.com>
2022-07-11 13:23:01 +0200

    doc generation finally works

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
           ];
           
 
-          buildFlags = [ "PATH_TO_COMPILED_LIBRARY=${self.outputs.packages.${system}.coq-matching-logic}/lib/coq/${coq.coq-version}/user-contrib/.MatchingLogic" ];
+          buildFlags = [ "PATH_TO_COMPILED_LIBRARY=${self.outputs.packages.${system}.coq-matching-logic}/lib/coq/${coq.coq-version}/user-contrib/MatchingLogic" ];
 
           installTargets = "install-coqdoc";
           # We are not going to install this system-wide, so we can afford not to have the coq version in the path to the docs.
