11c20546690bf4c9f1a1991d136bb5ff1ca03cbc
Jan Tusil <jenda.tusil@gmail.com>
2022-07-12 07:44:55 +0200

    Nix Flakes; CI improvements (#268)
    * remove old nix stuff
    
    * wrappers for nix flakes
    
    * fix directory
    
    * working devShell
    
    * build & install
    
    * WIP
    
    * rename
    
    * this is a test
    
    * remove confusing Makefile
    
    * WIP
    
    * move stuff around
    
    * ML example: FOL
    
    * coqdoc; mm-exporter
    
    * I do not know what I am doing
    
    * CoqDoc; cleanup
    
    * forgotten goal
    
    * update README.md
    
    * default + devShell
    
    * fix the compatability layer
    
    * remove old nix files
    
    * attempt to install doc
    
    * matching-logic-doc
    
    * fix the doc build
    
    * doc generation finally works
    
    * try CI
    
    * nix-command
    
    * flakes
    
    * print build logs
    
    * fix persisting to workspace
    
    * fix a typo
    
    * dependencies + patch shebang
    
    * nix-env -iA 'nixos.ghp-import'
    
    * nixpkgs
    
    * test
    
    * FIX DOC

--- Makefile
+++
@@ -1,7 +0,0 @@
-all: library prover
-
-library:
-	$(MAKE) -C matching-logic
-
-prover:
-	$(MAKE) -C prover
--- matching-logic/shell.nix
+++ matching-logic/shell.nix
@@ -1,14 +1,7 @@
-{ coqVersion ? "8.15"}:
-with import <nixpkgs> {};
-
-let
-
-  deps = import ./nix/deps.nix { inherit coqVersion; };
-  
-  self = mkShell {
-    name="ml-in-coq-dev-env";
-    buildInputs = [deps.coq deps.stdpp deps.equations];
-  };
-
-in
-  self
+(import (
+  fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/12c64ca55c1014cdc1b16ed5a804aa8576601ff2.tar.gz";
+    sha256 = "0jm6nzb83wa6ai17ly9fzpqc40wg1viib8klq8lby54agpl213w5"; }
+) {
+  src =  ../.;
+}).shellNix
