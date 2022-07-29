99096e5f97efb4b5ea6e74c332f7fda0ce26e84a
berpeti <peti.ber007@gmail.com>
2022-07-29 11:22:20 +0200

    Extend classes

--- matching-logic/src/SyntacticConstruct.v
+++ matching-logic/src/SyntacticConstruct.v
@@ -32,8 +32,10 @@ Print free_evar_subst.
 Check bevar_subst_exists.
 Search free_evar_subst patt_exists.
 Class EBinder (ebinder : Pattern -> Pattern)
-    (fevo: db_index -> Pattern -> Pattern -> Pattern )
-    (fsvo: db_index -> Pattern -> Pattern -> Pattern )
+    (fevo: db_index -> Pattern -> Pattern -> Pattern)
+    (fsvo: db_index -> Pattern -> Pattern -> Pattern)
+    (feq: evar -> db_index -> Pattern -> Pattern)
+    (fsq: svar -> db_index -> Pattern -> Pattern)
     :=
 {
 ebinder_bevar_subst :
@@ -48,21 +50,47 @@ ebinder_bsvar_subst :
       bsvar_subst (ebinder ϕ) ψ n = fsvo n ψ ϕ ;
 ebinder_free_evar_subst :
   forall ψ φ x,
-    (ebinder φ).[[evar: x ↦ ψ]] = ebinder (φ.[[evar: x ↦ ψ]])
+    (ebinder φ).[[evar: x ↦ ψ]] = ebinder (φ.[[evar: x ↦ ψ]]);
+ebinder_free_svar_subst :
+  forall ψ φ x,
+    (ebinder φ).[[svar: x ↦ ψ]] = ebinder (φ.[[svar: x ↦ ψ]]);
+ebinder_evar_quantify :
+  forall φ x n,
+    evar_quantify x n (ebinder φ) = feq x n φ;
+ebinder_svar_quantify :
+  forall φ x n,
+    svar_quantify x n (ebinder φ) = fsq x n φ;
 }.
 
-Class SBinder (sbinder : Pattern -> Pattern) :=
+Class SBinder (sbinder : Pattern -> Pattern)
+    (fevo: db_index -> Pattern -> Pattern -> Pattern)
+    (fsvo: db_index -> Pattern -> Pattern -> Pattern)
+    (feq: evar -> db_index -> Pattern -> Pattern)
+    (fsq: svar -> db_index -> Pattern -> Pattern)
+:=
 {
 sbinder_bevar_subst :
   forall ψ,
     well_formed_closed ψ ->
-    forall n ϕ,
-      bevar_subst (sbinder ϕ) ψ n = sbinder (bevar_subst ϕ ψ n) ;
+    forall n φ,
+      bevar_subst (sbinder φ) ψ n = fevo n ψ φ ;
 sbinder_bsvar_subst :
   forall ψ,
     well_formed_closed ψ ->
-    forall n ϕ,
-      bsvar_subst (sbinder ϕ) ψ n = sbinder (bsvar_subst ϕ ψ (S n)) ;
+    forall n φ,
+      bsvar_subst (sbinder φ) ψ n = fsvo n ψ φ ;
+sbinder_free_evar_subst :
+  forall ψ φ x,
+    (sbinder φ).[[evar: x ↦ ψ]] = sbinder (φ.[[evar: x ↦ ψ]]);
+sbinder_free_svar_subst :
+  forall ψ φ x,
+    (sbinder φ).[[svar: x ↦ ψ]] = sbinder (φ.[[svar: x ↦ ψ]]);
+sbinder_evar_quantify :
+  forall φ x n,
+    evar_quantify x n (sbinder φ) = feq x n φ;
+sbinder_svar_quantify :
+  forall φ x n,
+    svar_quantify x n (sbinder φ) = fsq x n φ;
 }.
 
 (* Non-variable nullary operation *)
@@ -78,6 +106,18 @@ nvnullary_bsvar_subst :
     well_formed_closed ψ ->
     forall n,
       bevar_subst nvnullary ψ n = nvnullary ;
+nvnullary_free_evar_subst :
+  forall ψ x,
+    nvnullary.[[evar: x ↦ ψ]] = nvnullary;
+nvnullary_free_svar_subst :
+  forall ψ x,
+    nvnullary.[[svar: x ↦ ψ]] = nvnullary;
+nvnullary_evar_quantify :
+  forall x n,
+    evar_quantify x n nvnullary = nvnullary;
+nvnullary_svar_quantify :
+  forall x n,
+    svar_quantify x n nvnullary = nvnullary;
 
 nvnullary_wf : well_formed nvnullary = true ;
 }.
@@ -94,6 +134,18 @@ unary_bsvar_subst :
     well_formed_closed ψ ->
     forall n ϕ,
       bsvar_subst (patt ϕ) ψ n = patt (bsvar_subst ϕ ψ n) ;
+unary_free_evar_subst :
+  forall φ ψ x,
+    (patt φ).[[evar: x ↦ ψ]] = patt φ.[[evar: x ↦ ψ]];
+unary_free_svar_subst :
+  forall φ ψ x,
+    (patt φ).[[svar: x ↦ ψ]] = patt φ.[[svar: x ↦ ψ]];
+unary_evar_quantify :
+  forall φ x n,
+    evar_quantify x n (patt φ) = patt (evar_quantify x n φ);
+unary_svar_quantify :
+  forall φ x n,
+    svar_quantify x n (patt φ) = patt (svar_quantify x n φ);
 
 unary_wf : forall ψ, well_formed ψ -> well_formed (patt ψ) ;
 }.
@@ -110,6 +162,18 @@ binary_bsvar_subst :
     well_formed_closed ψ ->
     forall n ϕ₁ ϕ₂,
       bsvar_subst (binary ϕ₁ ϕ₂) ψ n = binary (bsvar_subst ϕ₁ ψ n) (bsvar_subst ϕ₂ ψ n) ;
+binary_free_evar_subst :
+  forall φ₁ φ₂ ψ x,
+    (binary φ₁ φ₂).[[evar: x ↦ ψ]] = binary (φ₁.[[evar: x ↦ ψ]]) (φ₂.[[evar: x ↦ ψ]]);
+binary_free_svar_subst :
+  forall φ₁ φ₂ ψ x,
+    (binary φ₁ φ₂).[[svar: x ↦ ψ]] = binary (φ₁.[[svar: x ↦ ψ]]) (φ₂.[[svar: x ↦ ψ]]);
+binary_evar_quantify :
+  forall φ₁ φ₂ x n,
+    evar_quantify x n (binary φ₁ φ₂) = binary (evar_quantify x n φ₁) (evar_quantify x n φ₂);
+binary_svar_quantify :
+  forall φ₁ φ₂ x n,
+    svar_quantify x n (binary φ₁ φ₂) = binary (svar_quantify x n φ₁) (svar_quantify x n φ₂);
 
 binary_wf : forall ψ1 ψ2, well_formed ψ1 -> well_formed ψ2 -> well_formed (binary ψ1 ψ2) ;
 }.
