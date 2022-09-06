bcfa2c095152bddbb2af8700858d6c2cfa6b0976
Jan Tusil <jenda.tusil@gmail.com>
2022-09-06 20:09:45 +0200

    Do not copy assets

--- matching-logic-doc/run_alectryon.sh
+++ matching-logic-doc/run_alectryon.sh
@@ -4,13 +4,18 @@ path_to_compiled_library="$1"
 source_filename="$2"
 
 stripped_filename="${source_filename#${path_to_compiled_library}}"
-out_dir="doc/alectryon/$(dirname "$stripped_filename")"
-mkdir -p "$out_dir"
+base_out_dir="doc/alectryon/"
+out_dir="$base_out_dir/$(dirname "$stripped_filename")"
 
+
+set -x
+mkdir -p "$out_dir"
 alectryon -R "$path_to_compiled_library" MatchingLogic \
         --traceback \
         --frontend coq \
         --backend webpage \
-        --output-directory "$out_dir" \
+        --output-directory "$base_out_dir" \
+        --copy-assets none \
+        --output "$base_out_dir/$stripped_filename.html" \
         "$source_filename"
 
