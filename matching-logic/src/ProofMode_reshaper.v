94772c4ba96f50c5733272aea6cbc064127c9da8
Jan Tusil <jenda.tusil@gmail.com>
2022-09-10 16:51:53 +0200

    Reshaper: lhs_imp_to_and_r

--- matching-logic/src/ProofMode_reshaper.v
+++ matching-logic/src/ProofMode_reshaper.v
@@ -166,4 +166,127 @@ Section with_signature.
     apply lhs_and_to_imp_r.
   Abort.
 
+
+  Structure AndReshapeS (g : Pattern) (l : list Pattern) :=
+  AndReshape
+    { ars_flattened :> TaggedPattern ;
+      ars_pf : (untagPattern ars_flattened) = foldr patt_and g l ;
+    }.
+
+  Lemma AndReshape_nil_pf0:
+    ∀ (g : Pattern),
+      g = foldr patt_and g [].
+  Proof. intros. reflexivity. Qed.
+
+  Canonical Structure AndReshape_nil (g : Pattern) : AndReshapeS g [] :=
+    AndReshape g [] (reshape_nil g) (AndReshape_nil_pf0 g).
+
+
+  Program Canonical Structure AndReshape_cons
+     (g x : Pattern) (xs : list Pattern) (r : AndReshapeS g xs)
+  : AndReshapeS g (x::xs) :=
+    AndReshape g (x::xs) (reshape_cons (x and untagPattern (reshape_cons r))) _.
+  Next Obligation.
+    intros g x xs r. simpl.
+    rewrite ars_pf.
+    reflexivity.
+  Qed.
+
+
+  Lemma reshape_and (Γ : Theory) (g : Pattern) (xs: list Pattern) (i : ProofInfo) :
+    forall (r : AndReshapeS g xs),
+       Γ ⊢i foldr (patt_and) g xs using i ->
+       Γ ⊢i (untagPattern (ars_flattened _ _ r)) using i.
+  Proof.
+    intros r [pf Hpf].
+    unshelve (eexists).
+    {
+      eapply cast_proof.
+      { rewrite ars_pf; reflexivity. }
+      exact pf.
+    }
+    {
+      simpl.
+      destruct Hpf as [Hpf2 Hpf3 Hpf4].
+      constructor.
+      {
+        rewrite elem_of_subseteq in Hpf2.
+        rewrite elem_of_subseteq.
+        intros x Hx.
+        specialize (Hpf2 x).
+        apply Hpf2. clear Hpf2.
+        rewrite elem_of_gset_to_coGset in Hx.
+        rewrite uses_of_ex_gen_correct in Hx.
+        rewrite elem_of_gset_to_coGset.
+        rewrite uses_of_ex_gen_correct.
+        rewrite indifferent_to_cast_uses_ex_gen in Hx.
+        exact Hx.
+      }
+      {
+        rewrite elem_of_subseteq in Hpf3.
+        rewrite elem_of_subseteq.
+        intros x Hx.
+        specialize (Hpf3 x).
+        apply Hpf3. clear Hpf3.
+        rewrite elem_of_gset_to_coGset in Hx.
+        rewrite uses_of_svar_subst_correct in Hx.
+        rewrite elem_of_gset_to_coGset.
+        rewrite uses_of_svar_subst_correct.
+        rewrite indifferent_to_cast_uses_svar_subst in Hx.
+        exact Hx.
+      }
+      {
+        rewrite indifferent_to_cast_uses_kt.
+        apply Hpf4.
+      }
+      {
+        rewrite framing_patterns_cast_proof.
+        destruct i;assumption.
+      }
+    }
+  Defined.
+
+  Local Example ex_reshape_and Γ a b c d:
+  well_formed a ->
+  well_formed b ->
+  well_formed c ->
+  well_formed d ->
+  Γ ⊢i a and b and c and d using BasicReasoning.
+  Proof.
+    intros wfa wfb wfc wfd.
+    apply reshape_and.
+  (* Now the goal has the right shape *)
+  Abort.
+
+    (*
+      Γ ⊢ φ₁ ---> ... ---> φₖ ---> ψ
+      ------------------------------
+      Γ ⊢ (φ₁ and ... and φₖ) ---> ψ
+    *)
+  Lemma lhs_imp_to_and_r Γ (g x : Pattern) (xs : list Pattern) i :
+    well_formed g ->
+    well_formed x ->
+    Pattern.wf xs ->
+    forall (r : AndReshapeS x xs),
+       Γ ⊢i ((foldr (patt_imp) g (x::xs))) using i ->
+       Γ ⊢i (untagPattern (ars_flattened _ _ r)) ---> g using i .
+  Proof.
+    intros wfg wfx wfxs r H.
+    eapply cast_proof'.
+    { rewrite ars_pf; reflexivity. }
+    clear r.
+    apply lhs_imp_to_and_meta; assumption.
+  Defined.
+
+  Local Example ex_match_and Γ a b c d:
+   well_formed a ->
+   well_formed b ->
+   well_formed c ->
+   well_formed d ->
+   Γ ⊢i (a and b and c) ---> d using BasicReasoning.
+  Proof.
+   intros wfa wfb wfc wfd.
+   apply lhs_imp_to_and_r; simpl.
+  Abort.
+
 End with_signature.
