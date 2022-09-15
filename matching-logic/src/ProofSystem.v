30c7f448a11b77dd7c9fdef1886bbbb4c550f8fb
Jan Tusil <jenda.tusil@gmail.com>
2022-09-15 13:57:59 +0200

    get rid of one dependency

--- matching-logic/src/ProofSystem.v
+++ matching-logic/src/ProofSystem.v
@@ -2,8 +2,7 @@ From Coq Require Import ssreflect ssrfun ssrbool.
 
 From Coq Require Import Logic.Classical_Prop Logic.Eqdep_dec.
 From MatchingLogic.Utils Require Import stdpp_ext Lattice.
-(* TODO do we need [monotonic] here? *)
-From MatchingLogic Require Import Syntax NamedAxioms DerivedOperators_Syntax monotonic wftactics.
+From MatchingLogic Require Import Syntax NamedAxioms DerivedOperators_Syntax wftactics.
 From stdpp Require Import base fin_sets sets propset gmap.
 
 From MatchingLogic.Utils Require Import extralibrary.
