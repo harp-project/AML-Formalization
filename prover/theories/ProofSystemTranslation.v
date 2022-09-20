9d03302f12ad801c5342f7c20e4d3d46b7a3ba03
Jan Tusil <jenda.tusil@gmail.com>
2022-09-20 16:52:41 +0200

    WIP alpha equivalence relation

--- prover/theories/ProofSystemTranslation.v
+++ prover/theories/ProofSystemTranslation.v
@@ -3,7 +3,7 @@ From Coq Require Import ssreflect ssrfun ssrbool.
 From Coq Require Import Strings.String.
 From Equations Require Import Equations.
 
-From stdpp Require Export base.
+From stdpp Require Export base gmap.
 From MatchingLogic Require Import Syntax Semantics StringSignature ProofSystem ProofMode.
 From MatchingLogicProver Require Import Named NamedProofSystem NMatchers.
 
@@ -30,6 +30,211 @@ Ltac case_match_in_hyp H :=
   | context [ match ?x with _ => _ end ] => destruct x eqn:?
   end.
 
+
+(* In an empty context, this rewrites: *)
+(* ∃ (x.x) /\ (∃ y.y) ~> (∃ x.x) /\ (∃ x.x) *)
+(* But in some other context C, it might rewrite *)
+(* C[∃ (x.x) /\ (∃ y.y)] ~> C[(∃ z.z) /\ (∃ z.z)] *)
+(* because we might see some ∃ z.z in the context C. *)
+
+
 (* TODO we have to implement the two-phase approach to proof system translation.
    The cache-based on was too complicated, so I removed the proofs from this file.
+*)
+(*
+Record AlphaConversion {Σ : Signature} :=
+  {
+    ac_list : list evar ;
+  }.
+
+Definition AlphaConversion_valid {Σ : Signature}
+  (ac : AlphaConversion) (nϕ : NamedPattern)
+  := (ac_list ac) ## elements (named_free_evars nϕ).
+
+Print NamedPattern.
+*)
+
+(*
+Definition head_or_id {Σ : Signature}
+  (l : list evar) (p : NamedPattern) : NamedPattern 
+  match l with
+  | [] => p
+  | (x::xs) => 
+*)
+
+Fixpoint number_of_exists
+  {Σ : Signature} (ϕ : NamedPattern) : nat :=
+  match ϕ with
+  | npatt_app phi1 phi2
+    => number_of_exists phi1 + number_of_exists phi2
+  | npatt_imp phi1 phi2
+    => number_of_exists phi1 + number_of_exists phi2
+  | npatt_exists _ phi
+    => S (number_of_exists phi)
+  | npatt_mu _ phi
+    => number_of_exists phi
+  | _ => 0
+  end.
+
+  Fixpoint number_of_mus
+  {Σ : Signature} (ϕ : NamedPattern) : nat :=
+  match ϕ with
+  | npatt_app phi1 phi2
+    => number_of_mus phi1 + number_of_mus phi2
+  | npatt_imp phi1 phi2
+    => number_of_mus phi1 + number_of_mus phi2
+  | npatt_exists _ phi
+    => (number_of_mus phi)
+  | npatt_mu _ phi
+    => S ( number_of_mus phi)
+  | _ => 0
+  end.
+
+Fixpoint rename {Σ : Signature}
+  (bevs : list evar)
+  (bsvs : list svar)
+  (fevs : gmap evar evar)
+  (fsvs : gmap svar svar)
+  (nϕ : NamedPattern)
+  : NamedPattern :=
+  match nϕ with
+  | npatt_evar x
+    => match (fevs !! x) with
+       | None => npatt_evar x
+       | Some y => npatt_evar y
+       end
+  | npatt_svar X
+       => match (fsvs !! X) with
+          | None => npatt_svar X
+          | Some Y => npatt_svar Y
+          end
+  | npatt_sym s => npatt_sym s
+  | npatt_bott => npatt_bott
+  | npatt_imp phi1 phi2 =>
+    npatt_imp
+      (rename bevs bsvs fevs fsvs phi1)
+      (rename (drop (number_of_exists phi1) bevs)
+              (drop (number_of_mus phi1) bsvs)
+              fevs fsvs phi2) 
+  | npatt_app phi1 phi2 =>
+    npatt_app
+      (rename bevs bsvs fevs fsvs phi1)
+      (rename (drop (number_of_exists phi1) bevs)
+      (drop (number_of_exists phi1) bsvs)
+              fevs fsvs phi2) 
+  | npatt_exists x phi =>
+    match bevs with
+    | [] => npatt_exists x phi
+    | (y::ys)
+      => npatt_exists y
+          (rename ys bsvs (<[x:=y]>fevs) fsvs phi)
+    end
+  | npatt_mu X phi =>
+    match bsvs with
+    | [] => npatt_mu X phi
+    | (Y::Ys)
+      => npatt_mu Y
+          (rename bevs Ys fevs (<[X:=Y]>fsvs) phi)
+    end
+  end.
+
+
+  Record PartialBijection (A : Type)
+    {_eqd : EqDecision A}
+    {_cnt : Countable A}
+     :=
+  {
+    pbr : gset (prod A A) ;
+    pb1 : forall (x y1 y2 : A),
+      (x,y1) ∈ pbr -> (x,y2) ∈ pbr -> y1 = y2 ;
+    pb2 : forall (x1 x2 y : A),
+      (x1,y) ∈ pbr -> (x2,y) ∈ pbr -> x1 = x2 ;
+  }.
+
+  Arguments pbr {A%type_scope} {_eqd _cnt} p.
+
+  Definition related {A : Type} (u v : prod A A) : Prop
+  := u.1 = v.1 \/ u.2 = v.2.
+
+  Definition unrelated {A : Type} (u v : prod A A) : Prop
+  := ~ (related u v).
+
+  #[global]
+  Instance related_dec (A : Type)
+    {eq_dec : EqDecision A}
+  : RelDecision (@unrelated A).
+  Proof.
+    solve_decision.
+  Defined.
+
+  Program Definition pb_update
+  {A : Type}
+  {_eqd : EqDecision A}
+  {_cnt : Countable A}
+  (pb : PartialBijection A)
+  (x y : A)
+  : PartialBijection A := {|
+    pbr := (filter (unrelated (x,y)) (pbr pb) ) ∪ {[ (x,y) ]} ;
+  |}.
+  Next Obligation.
+    intros ???? ????? H1 H2.
+    rewrite elem_of_union in H1.
+    rewrite elem_of_union in H2.
+    rewrite elem_of_singleton in H1.
+    rewrite elem_of_singleton in H2.
+    rewrite elem_of_filter in H1.
+    rewrite elem_of_filter in H2.
+    destruct pb.
+    unfold unrelated,related in *.
+    simpl in *.
+    naive_solver.
+  Qed.
+ Next Obligation.
+  intros ???? ????? H1 H2.
+  rewrite elem_of_union in H1.
+  rewrite elem_of_union in H2.
+  rewrite elem_of_singleton in H1.
+  rewrite elem_of_singleton in H2.
+  rewrite elem_of_filter in H1.
+  rewrite elem_of_filter in H2.
+  destruct pb.
+  unfold unrelated,related in *.
+  simpl in *.
+  naive_solver.
+ Qed.
+
+  Inductive alpha_equiv
+    {Σ : Signature}
+    (R : PartialBijection evar)
+    (R' : PartialBijection svar)
+    : relation NamedPattern
+    :=
+    | ae_evar (x y : evar) (pf : (x, y) ∈ (pbr R))
+      : alpha_equiv R R' (npatt_evar x) (npatt_evar y)
+    | ae_svar (X Y : svar) (pf : (X, Y) ∈ (pbr R'))
+      : alpha_equiv R R' (npatt_svar X) (npatt_svar Y)
+    | ae_app
+      (t t' u u' : NamedPattern)
+      (tEt' : alpha_equiv R R' t t')
+      (uEu' : alpha_equiv R R' u u')
+      : alpha_equiv R R' (npatt_app t u) (npatt_app t' u')
+    | ae_imp
+      (t t' u u' : NamedPattern)
+      (tEt' : alpha_equiv R R' t t')
+      (uEu' : alpha_equiv R R' u u')
+      : alpha_equiv R R' (npatt_imp t u) (npatt_imp t' u')
+    | ae_bott : alpha_equiv R R' npatt_bott npatt_bott
+    | ae_sym (s : symbols) : alpha_equiv R R' (npatt_sym s) (npatt_sym s) 
+    .
+
+  (*
+  (* TESTS *)
+From MatchingLogic Require Import StringSignature.
+
+Definition Σ : Signature :=
+  {|
+   |}
+#[local]
+Example ex_rename {Σ : Signature} :
+  rename  = patt_bott.
 *)
