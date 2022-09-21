d47b08f065945b162437e76fa3ae3324e8d62196
Jan Tusil <jenda.tusil@gmail.com>
2022-09-21 18:07:34 +0200

    variable_occurs_positively

--- matching-logic/src/PatternContext.v
+++ matching-logic/src/PatternContext.v
@@ -32,5 +32,31 @@ Section with_signature.
   Definition emplace (ctx : PatternCtx) (p : Pattern) : Pattern :=
     (pcPattern ctx)^[[evar: (pcEvar ctx) ↦ p]].
 
-
+  Fixpoint variable_occurs_positively (x : evar) (phi : Pattern) : bool :=
+    match phi with
+    | patt_free_evar y => match decide (x = y) with left _ => true | right _ => false end
+    | patt_free_svar X => false
+    | patt_bound_evar _ => false
+    | patt_bound_svar _ => false                             
+    | patt_bott => false
+    | patt_sym _ => false
+    | patt_imp phi1 phi2 => (variable_occurs_negatively x phi1) || (variable_occurs_positively x phi2)
+    | patt_app phi1 phi2 => (variable_occurs_positively x phi1) || (variable_occurs_positively x phi2)
+    | patt_exists phi' => variable_occurs_positively x phi'
+    | patt_mu phi' => variable_occurs_positively x phi'
+    end
+  with variable_occurs_negatively (x : evar) (phi : Pattern) : bool :=
+         match phi with
+         | patt_free_evar y => false
+         | patt_free_svar X => false
+         | patt_bound_evar _ => false
+         | patt_bound_svar _ => false                             
+         | patt_bott => false
+         | patt_sym _ => false
+         | patt_imp phi1 phi2 => (variable_occurs_positively x phi1) || (variable_occurs_negatively x phi2)
+         | patt_app phi1 phi2 => (variable_occurs_negatively x phi1) || (variable_occurs_negatively x phi2)
+         | patt_exists phi' => variable_occurs_negatively x phi'
+         | patt_mu phi' => variable_occurs_negatively x phi'         
+    end.
+  
 End with_signature.
