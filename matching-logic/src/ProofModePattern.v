c83f001c6bf1de57a0e1705ae13ae0f4b0b88c6f
Jan Tusil <jenda.tusil@gmail.com>
2023-02-06 18:36:21 +0100

    WIP just playing around

--- matching-logic/src/ProofModePattern.v
+++ matching-logic/src/ProofModePattern.v
@@ -30,6 +30,37 @@ B :=
 .
 
 Definition VarName := string.
+Definition EVarName := VarName.
+Definition SVarName := VarName.
+
+
+Definition incr_values
+    (vm : gmap VarName nat)
+    : gmap VarName nat
+:= fmap (fun n => S n) vm.
+
+
+Record PContext := mkPContext {
+    pc_evm : gmap EVarName nat ;
+    pc_svm : gmap SVarName nat ;
+}.
+
+Definition pc_add_ename
+    (ctx : PContext)
+    (name : EVarName) :=
+    mkPContext ((<[name := 0]>(incr_values (pc_evm ctx)))) (pc_svm ctx)
+.
+
+Definition pc_add_sname
+    (ctx : PContext)
+    (name : SVarName) :=
+    mkPContext (pc_evm ctx) ((<[name := 0]>(incr_values (pc_svm ctx))))
+.
+
+Definition Pattern_in_context
+    {Σ : Signature} :=
+    PContext -> Pattern
+.
 
 (*
  So what notations we want to support?
@@ -41,40 +72,17 @@ Definition VarName := string.
  (2) how many element variables it binds
  (3) how many set variables it binds.
 *)
-Record MLNotation {Σ : Signature} := {
-    (*mln_level : nat ;*)
-    mln_arity : nat ;
-    mln_ebinder_arity : nat;
-    mln_sbinder_arity : nat;
-    mln_expand :
-      vec Pattern mln_arity -> (* patterns *)
-      vec VarName mln_ebinder_arity -> (* bound evars*)
-      vec VarName mln_sbinder_arity -> (* bound svars*)
-      Pattern ;
+Record MLConstruct {Σ : Signature} := {
+    (*mlc_level : nat ;*)
+    mlc_arity : nat ;
+    mlc_ebinder_arity : nat;
+    mlc_sbinder_arity : nat;
+    mlc_expand :
+      vec Pattern_in_context mlc_arity -> (* patterns *)
+      vec VarName mlc_ebinder_arity -> (* bound evars*)
+      vec VarName mlc_sbinder_arity -> (* bound svars*)
+      Pattern_in_context ;
 }.
-
-Inductive PMPattern {Σ : Signature} : Set :=
-(* Direct constructors of a ML pattern *)
-| pmpatt_evar (x : string)
-| pmpatt_svar (X : string)
-| pmpatt_sym (sigma : symbols)
-| pmpatt_app (phi1 phi2 : PMPattern)
-| pmpatt_bott
-| pmpatt_imp (phi1 phi2 : PMPattern)
-| pmpatt_exists (x : string) (phi : PMPattern)
-| pmpatt_mu (X : string) (phi : PMPattern)
-(* Indirect constructor - through notations  *)
-| pmpatt_notation
-    (notation : MLNotation)
-    (args : vec PMPattern (mln_arity notation))
-    (bound_evars : vec VarName (mln_ebinder_arity notation))
-    (bound_svars : vec VarName (mln_sbinder_arity notation))
-.
-
-Definition incr_values
-    (vm : gmap VarName nat)
-    : gmap VarName nat
-:= fmap (fun n => S n) vm.
 
 Section sec.
     Context
@@ -84,9 +92,234 @@ Section sec.
         {string2svar : string -> svar}
         {string2svar_inj : Inj (=) (=) string2svar}
     .
+    Definition mlc_sym
+        (s : symbols)
+        : MLConstruct := {|
+            mlc_arity := 0 ;
+            mlc_ebinder_arity := 0 ;
+            mlc_sbinder_arity := 0 ;
+            mlc_expand :=
+                fun _ =>
+                fun _ =>
+                fun _ => (
+                    fun _ =>
+                    patt_sym s
+                )
+        |}
+    .
+
+    Definition mlc_bott
+        : MLConstruct := {|
+            mlc_arity := 0 ;
+            mlc_ebinder_arity := 0 ;
+            mlc_sbinder_arity := 0 ;
+            mlc_expand :=
+                fun _ =>
+                fun _ =>
+                fun _ => (
+                    fun _ =>
+                    patt_bott
+                )
+        |}
+    .
+        
+
+    Definition mlc_evar
+        {name : EVarName}
+        : MLConstruct := {|
+            mlc_arity := 0 ;
+            mlc_ebinder_arity := 0 ;
+            mlc_sbinder_arity := 0 ;
+            mlc_expand :=
+                fun _ =>
+                fun _ =>
+                fun _ => (
+                    fun ctx =>
+                    match (pc_evm ctx) !! name with
+                    | Some n => patt_bound_evar n
+                    | None => patt_free_evar (string2evar name)
+                    end
+                )
+        |}
+    .
+
+    Definition mlc_svar
+        {name : SVarName}
+        : MLConstruct := {|
+            mlc_arity := 0 ;
+            mlc_ebinder_arity := 0 ;
+            mlc_sbinder_arity := 0 ;
+            mlc_expand :=
+                fun _ =>
+                fun _ =>
+                fun _ => (
+                    fun ctx =>
+                    match (pc_svm ctx) !! name with
+                    | Some n => patt_bound_svar n
+                    | None => patt_free_svar (string2svar name)
+                    end
+                )
+        |}
+    .
+
+    Definition mlc_app
+        : MLConstruct := {|
+            mlc_arity := 2 ;
+            mlc_ebinder_arity := 0 ;
+            mlc_sbinder_arity := 0 ;
+            mlc_expand :=
+                fun ps =>
+                fun _ =>
+                fun _ => (
+                    fun ctx =>
+                    patt_app
+                        ((ps !!! (0)%fin) ctx)
+                        ((ps !!! (1)%fin) ctx)
+                )
+        |}
+    .
+
+    Definition mlc_imp
+        : MLConstruct := {|
+            mlc_arity := 2 ;
+            mlc_ebinder_arity := 0 ;
+            mlc_sbinder_arity := 0 ;
+            mlc_expand :=
+                fun ps =>
+                fun _ =>
+                fun _ => (
+                    fun ctx =>
+                    patt_imp
+                        ((ps !!! (0)%fin) ctx)
+                        ((ps !!! (1)%fin) ctx)
+                )
+        |}
+    .
+
+    Definition mlc_exists
+        : MLConstruct := {|
+            mlc_arity := 1 ;
+            mlc_ebinder_arity := 1 ;
+            mlc_sbinder_arity := 0 ;
+            mlc_expand :=
+                fun ps =>
+                fun evs =>
+                fun _ => (
+                    fun ctx =>
+                        let name := evs !!! (0)%fin in
+                        let ctx' := pc_add_ename ctx name in
+                        let ϕ_in_context := ps !!! (0)%fin in
+                        let ϕ := ϕ_in_context ctx' in
+                        patt_exists ϕ
+                )
+        |}
+    .
+
+    Definition mlc_mu
+        : MLConstruct := {|
+            mlc_arity := 1 ;
+            mlc_ebinder_arity := 0 ;
+            mlc_sbinder_arity := 1 ;
+            mlc_expand :=
+                fun ps =>
+                fun _ =>
+                fun svs => (
+                    fun ctx =>
+                        let name := svs !!! (0)%fin in
+                        let ctx' := pc_add_sname ctx name in
+                        let ϕ_in_context := ps !!! (0)%fin in
+                        let ϕ := ϕ_in_context ctx' in
+                        patt_mu ϕ
+                )
+        |}
+    .
+
+
+(*
+  Example:
+  ∀ x. ∃ y. x ---> y
+  ∀ x. ∃ y. x /\ y
+*)
+
+Inductive PMPattern {Σ : Signature} : Set :=
+| pmpatt_mu (X : string) (phi : PMPattern)
+| pmpatt_construct
+    (construct : MLConstruct)
+    (args : vec PMPattern (mlc_arity construct))
+    (bound_evars : vec VarName (mlc_ebinder_arity construct))
+    (bound_svars : vec VarName (mlc_sbinder_arity construct))
+.
+
+Equations? PMPattern_size
+    {Σ : Signature}
+    (ϕ : PMPattern)
+    : nat :=
+    PMPattern_size (pmpatt_evar _) := 1 ;
+    PMPattern_size (pmpatt_svar _) := 1 ;
+    PMPattern_size (pmpatt_sym _) := 1 ;
+    PMPattern_size (pmpatt_bott) := 1 ;
+    PMPattern_size (pmpatt_app ϕ₁ ϕ₂) :=
+        S ((PMPattern_size ϕ₁) + (PMPattern_size ϕ₂)) ;
+    PMPattern_size (pmpatt_imp ϕ₁ ϕ₂) :=
+        S ((PMPattern_size ϕ₁) + (PMPattern_size ϕ₂)) ;
+    PMPattern_size (pmpatt_exists _ ϕ') := 
+        S (PMPattern_size ϕ') ;
+    PMPattern_size (pmpatt_mu _ ϕ') := 
+        S (PMPattern_size ϕ') ;
+    PMPattern_size (pmpatt_notation _ args _ _) :=
+        PMPatt
+    .
+
+
+Check sum_list.
+Check vec_to_list.
+Fixpoint PMPattern_size
+    {Σ : Signature}
+    (ϕ : PMPattern)
+    : nat :=
+    match ϕ with
+    | pmpatt_evar _ => 1
+    | pmpatt_svar _ => 1
+    | pmpatt_sym _ => 1
+    | pmpatt_bott => 1
+    | pmpatt_app ϕ₁ ϕ₂ =>
+        1 + (PMPattern_size ϕ₁) + (PMPattern_size ϕ₂)
+    | pmpatt_imp ϕ₁ ϕ₂ =>
+        1 + (PMPattern_size ϕ₁) + (PMPattern_size ϕ₂)
+    | pmpatt_exists _ ϕ' => 1 + (PMPattern_size ϕ')
+    | pmpatt_mu _ ϕ' => 1 + (PMPattern_size ϕ')
+    | pmpatt_notation _ args _ _ =>
+        1 + sum_list (fmap PMPattern_size (vec_to_list args))
+    end
+.
+
+Equations Derive NoConfusion for PMPattern.
+Equations Derive Subterm for PMPattern.
+
+Check PMPattern_subterm.
+
 
     Check insert.
 
+    Print PMPattern_direct_subterm.
+
+    Equations? pmp2ln_aux 
+        (ϕ : PMPattern)
+    : Pattern by wf ϕ (PMPattern_direct_subterm Σ) :=
+    pmp2ln_aux (pmpatt_evar x) := patt_bound_evar 0 ;
+    pmp2ln_aux _ := patt_bound_evar 0 .
+
+
+    Equations? pmp2ln_aux 
+        (evm : gmap VarName nat)
+        (svm : gmap VarName nat)
+        (ϕ : PMPattern)
+    : Pattern by wf ϕ PMPattern_subterm :=
+    pmp2ln_aux evm svm (pmpatt_evar x) :=
+        match evm !! x with
+        | Some n => patt_bound_evar n
+        | None => patt_free_evar (string2evar x)
+        end.
 
 
     Fixpoint pmp2ln_aux
@@ -127,7 +360,7 @@ Section sec.
                     (<[name := 0]>(incr_values svm))
                     ϕ')
         | pmpatt_notation notation args bound_evars bound_svars =>
-            mln_expand
+            mlc_expand
                 notation
                 (vmap (fun ϕ' => pmp2ln_aux evm svm) args)
                 bound_evars
