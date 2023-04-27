77612aa6d42c38d2d55e54ac73e9f32052ebc130
Jan Tusil <jenda.tusil@gmail.com>
2023-04-27 15:15:19 +0200

    fresh set variables

--- matching-logic/src/FreshnessManager.v
+++ matching-logic/src/FreshnessManager.v
@@ -212,6 +212,138 @@ Proof.
 Qed.
     
 
+Lemma FreshMan_fresh_svar
+    {Σ : Signature}
+    (fm_patterns : list Pattern)
+    (fm_evars : list evar)
+    (fm_svars : list svar)
+    (FM : FreshnessManager fm_patterns fm_evars fm_svars)
+    :
+    { X : svar & (FreshnessManager fm_patterns fm_evars (X::fm_svars))}
+.
+Proof.
+    remember (free_svars <$> fm_patterns) as lsvs.
+    remember ((@elements svar SVarSet _) <$> lsvs) as llsvs.
+    remember (mjoin llsvs) as svs.
+    remember (svar_fresh (fm_svars ++ svs)) as X.
+
+    assert (He1: forall (Y : svar), Y ∈ svs -> X <> Y).
+    {
+        intros Y HY HContra.
+        subst Y.
+        subst X.
+        eapply not_elem_of_larger_impl_not_elem_of in HY.
+        3: {
+            apply set_svar_fresh_is_fresh''.
+        }
+        2: {
+            clear. set_solver.
+        }
+        {
+            exact HY.
+        }
+    }
+
+    assert (He2: forall (Y : svar), Y ∈ fm_svars -> X <> Y).
+    {
+        intros Y HY HContra.
+        subst Y.
+        subst X.
+        eapply not_elem_of_larger_impl_not_elem_of in HY.
+        3: {
+            apply set_svar_fresh_is_fresh''.
+        }
+        2: {
+            clear. set_solver.
+        }
+        {
+            exact HY.
+        }
+    }
+
+    exists X.
+    constructor.
+    {
+        apply FM.
+    }
+    {
+        intros i j x0 y0 Hi Hj H.
+        destruct i,j; cbn in *.
+        {
+            inversion Hi; inversion Hj; subst x0 y0.
+            clear -H. contradiction.
+        }
+        {
+            inversion Hi. subst x0.
+            apply He2.
+            eapply elem_of_list_lookup_2.
+            apply Hj.
+        }
+        {
+            inversion Hj. subst y0.
+            apply nesym.
+            apply He2.
+            eapply elem_of_list_lookup_2.
+            apply Hi.
+        }
+        {
+            assert (i <> j) by lia.
+            destruct FM.
+            eapply fm_svars_nodup0.
+            { exact Hi. }
+            { exact Hj. }
+            { exact H0. }
+        }
+    }
+    {
+        apply FM.
+    }
+    {
+        intros i j x0 ϕ Hi Hj.
+        destruct i; cbn in *.
+        {
+            inversion Hi. subst x0.
+            unfold evar_is_fresh_in.
+            intros HContra.
+            assert (HContra': X ∈ elements (free_svars ϕ)).
+            {
+                rewrite elem_of_elements.
+                exact HContra.
+            }
+            assert (Hϕinfmp: ϕ ∈ fm_patterns).
+            {
+                apply elem_of_list_lookup.
+                exists j. apply Hj.
+            }
+            clear Hj.
+            subst svs.
+            setoid_rewrite elem_of_list_join in He1.
+            specialize (He1 X).
+            apply He1.
+            2: reflexivity.
+            exists (elements (free_svars ϕ)).
+            split.
+            {
+                exact HContra'.
+            }
+            subst llsvs.
+            unfold fmap.
+            rewrite elem_of_list_fmap.
+            eexists. split. reflexivity.
+            subst lsvs.
+            unfold fmap.
+            rewrite elem_of_list_fmap.
+            eexists. split. reflexivity.
+            exact Hϕinfmp.
+        }
+        {
+            eapply FM.
+            { apply Hi. }
+            { apply Hj. }
+        }
+    }
+Qed.
+
 
 Ltac _fm_new := set EmptyFreshMan as FM.
 Ltac _ensure_fm :=
@@ -231,6 +363,16 @@ Tactic Notation "mlFreshEvar" "as" ident(X) :=
     end
 .
 
+
+Tactic Notation "mlFreshSvar" "as" ident(X) :=
+    _ensure_fm;
+    lazymatch goal with
+    | [ FM : FreshnessManager ?ps ?evs ?svs |- _] =>
+        apply FreshMan_fresh_svar in FM;
+        destruct FM as [X FM]
+    end
+.
+
 Ltac2 rec index_of (x: constr) (l : constr) : constr
 :=
     lazy_match! l with
@@ -243,15 +385,21 @@ Ltac2 rec index_of (x: constr) (l : constr) : constr
     end
 .
 
-(* TODO a branch for set variables *)
 Ltac2 fm_distinct () :=
     match! goal with
     | [ fm : (FreshnessManager ?ps ?evs ?svs), x : evar, y : evar |- (?x <> ?y)] =>
-        Message.print (Message.of_string "Hello" );
         let i := (index_of x evs) in
         let j := (index_of y evs) in
         let fmt := (Control.hyp fm) in
         let pf := constr:(fm_evars_nodup $ps $evs $svs $fmt $i $j $x $y) in
+        Message.print (Message.of_constr i );
+        Message.print (Message.of_constr j );
+        apply $pf > [reflexivity|reflexivity|ltac1:(lia)]
+    | [ fm : (FreshnessManager ?ps ?evs ?svs), x : svar, y : svar |- (?x <> ?y)] =>
+        let i := (index_of x svs) in
+        let j := (index_of y svs) in
+        let fmt := (Control.hyp fm) in
+        let pf := constr:(fm_svars_nodup $ps $evs $svs $fmt $i $j $x $y) in
         Message.print (Message.of_constr i );
         Message.print (Message.of_constr j );
         apply $pf > [reflexivity|reflexivity|ltac1:(lia)]
@@ -267,6 +415,14 @@ Proof.
     mlFreshEvar as y.
     mlFreshEvar as z.
     assert (x <> z).
+    {
+        fm_distinct.
+    }
+
+    mlFreshSvar as X.
+    mlFreshSvar as Y.
+    mlFreshSvar as Z.
+    assert (Y <> Z).
     {
         fm_distinct.
     }
