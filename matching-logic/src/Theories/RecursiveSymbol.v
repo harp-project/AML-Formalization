9ab3a0dcb1a9a1c3c9bfcd60a8f7041f06eff004
Jan Tusil <jenda.tusil@gmail.com>
2023-07-12 15:32:21 +0200

    chain_patterns

--- matching-logic/src/Theories/RecursiveSymbol.v
+++ matching-logic/src/Theories/RecursiveSymbol.v
@@ -10,6 +10,7 @@ From MatchingLogic Require Import
     Theories.ProductSortWithLookup
 .
 
+Import Logic.Notations.
 Import BoundVarSugar.
 Import Definedness_Syntax.Notations.
 Import ProductSort.Notations.
@@ -20,33 +21,131 @@ Section rs.
     Context
         {Σ : Signature}
         {sorts_syntax : Sorts_Syntax.Syntax} 
-        (param_sort : list symbols)
-        (ret_sort : symbols)
     .
 
-    Inductive Intermediate :=
-    | i_leaf
-        (s_right : symbols)
-    | i_step
-        (s_left : symbols)
+    Definition apply_connective (connective arg1 arg2 : Pattern) : Pattern
+    := ((connective $ arg1) $ arg2)%ml.
+
+    Definition chain_patterns
+        (connective : Pattern)
+        (ps : list Pattern)
+        (last : Pattern)
+        : Pattern
+    :=
+        fold_right
+            (apply_connective connective)
+            (* (fun p acc => ((connective $ p) $ acc))%ml *)
+            last
+            ps
+    .
+
+    #[local] Example chain_1_patterns:
+        chain_patterns
+            (⊥%ml)
+            []
+            (b0)
+        = b0
+    .
+    Proof. reflexivity. Qed.
+
+    #[local] Example chain_2_patterns:
+        chain_patterns
+            (⊥%ml)
+            [b0]
+            (b1)
+        = ((⊥ $ b0) $ b1)%ml
+    .
+    Proof. reflexivity. Qed.
+
+    #[local] Example chain_3_patterns:
+        chain_patterns
+            (⊥%ml)
+            [b0;b1]
+            (b2)
+        = ((⊥ $ b0) $ ((⊥ $ b1) $ b2))%ml
+    .
+    Proof. reflexivity. Qed.
+
+    #[local] Example chain_4_patterns:
+        chain_patterns
+            (⊥%ml)
+            [b0;b1;b2]
+            (b3)
+        = ((⊥ $ b0) $ ((⊥ $ b1) $ ((⊥ $ b2) $ b3)))%ml
+    .
+    Proof. reflexivity. Qed.
+
+
+    Context
+        (param_sort : list Pattern)
+        (ret_sort : Pattern)
+    .
+
+    (*
+      This is going to be tricky.
+      We need to define those three operations on lists of things:
+      (1) s1 ⊗ · · · ⊗ sn ⊗ t ≡ s1 ⊗ (s2 ⊗ (· · · ⊗ (sn ⊗ t) . . . ))
+      (2) ⟨ϕ1, . . . , ϕn, ϕ⟩ ≡ ⟨ϕ1, ⟨. . . , ⟨ϕn, ϕ⟩ . . .⟩⟩
+      (3) ψ(ϕ1, . . . , ϕn ) ≡ ψ(ϕ1 ) . . . (ϕn ).
+      
+      Therefore, we need to have syntax for
+      sn ⊗ t, sn-1 ⊗ sn ⊗ t, etc.
+      By "syntax", we mean
+      `ProductSort.Syntax s1 s2`.
+
+      What sorts do we need?
+
+      Case length(param_sort)=0:
+        `ret_sort`
+
+      Case length(param_sort)=1:        
+        `ret_sort`
+        `param_sort!!0 ⊗ ret_sort`
+
+      Case length(param_sort)=2:        
+        `ret_sort`
+        `param_sort!!1 ⊗ ret_sort`
+        `param_sort!!0 ⊗ (param_sort!!1 ⊗ ret_sort)`
+
+      Case length(param_sort)=3:        
+        `ret_sort`
+        `param_sort!!2 ⊗ ret_sort`
+        `param_sort!!1 ⊗ (param_sort!!2 ⊗ ret_sort)`
+        `param_sort!!0 ⊗ (param_sort!!1 ⊗ (param_sort!!2 ⊗ ret_sort))`
+
+      Let n=3. We need the user to provide us with something like
+    *)
+
+    
+    Check fold_right.
+    Definition partial_sort_applications : list Pattern :=
+        fold_right (fun b a => a) ret_sort param_sort
+    .
+    Print fold_right.
+
+    Inductive Syntaxes :=
+    | sxs_leaf
+        (s_right : Pattern)
+    | sxs_step
+        (s_left : Pattern)
         (s_right : Pattern)
         (syntax: ProductSortWithLookup.Syntax s_left s_right)
-        (next : Intermediate)
+        (next : Syntaxes)
     .
 
-    Definition im_right (im : Intermediate) : Pattern :=
-        match im with
-        | i_leaf s_right => patt_sym s_right
-        | i_step s_left s_right syntax next =>
+    Definition sxs_right (sxs : Syntaxes) : Pattern :=
+        match sxs with
+        | sxs_leaf s_right => s_right
+        | sxs_step s_left s_right syntax next =>
             (mlProd(s_left, s_right))%ml    
         end
     .
 
-    Fixpoint im_valid (im : Intermediate) : Prop :=
-        match im with
-        | i_leaf _ => True
-        | i_step s_left s_right syntax next =>
-            s_right = im_right next
+    Fixpoint sxs_valid (sxs : Syntaxes) : Prop :=
+        match sxs with
+        | sxs_leaf _ => True
+        | sxs_step s_left s_right syntax next =>
+            s_right = sxs_right next /\ sxs_valid next
         end
     .
 
@@ -56,17 +155,7 @@ Section rs.
         i_
     .
     
-    (*
-      This is going to be tricky.
-      We need to define those three operations on lists of things:
-      (1) s1 ⊗ · · · ⊗ sn ⊗ t ≡ s1 ⊗ (s2 ⊗ (· · · ⊗ (sn ⊗ t) . . . ))
-      (2) ⟨ϕ1, . . . , ϕn, ϕ⟩ ≡ ⟨ϕ1, ⟨. . . , ⟨ϕn, ϕ⟩ . . .⟩⟩
-      (3) ψ(ϕ1, . . . , ϕn ) ≡ ψ(ϕ1 ) . . . (ϕn ).
-      However, for these we need to have an instance of
-      `ProductSort.Syntax s1 s2` but for any consecutive s1 s2 from the list.
 
-    
-    *)
 
 (*
 
