3d6667598570bdc30418efdf665c15b8cb99b201
Jan Tusil <jenda.tusil@gmail.com>
2023-11-09 09:55:48 +0100

    cleanup

--- matching-logic/src/Signature.v
+++ matching-logic/src/Signature.v
@@ -10,29 +10,29 @@ From stdpp Require Import
 Class MLVariables := {
   evar : Set;
   svar : Set;
-  evar_eqdec :> EqDecision evar;
-  evar_countable :> Countable evar;
-  evar_infinite :> Infinite evar;
-  svar_eqdec :> EqDecision svar;
-  svar_countable :> Countable svar;
-  svar_infinite :> Infinite svar;
+  evar_eqdec :: EqDecision evar;
+  evar_countable :: Countable evar;
+  evar_infinite :: Infinite evar;
+  svar_eqdec :: EqDecision svar;
+  svar_countable :: Countable svar;
+  svar_infinite :: Infinite svar;
 
   string2evar : string -> evar ;
-  string2evar_inj : Inj (=) (=) string2evar ;
+  string2evar_inj :: Inj (=) (=) string2evar ;
   string2svar : string -> svar ;
-  string2svar_inj : Inj (=) (=) string2svar ;
+  string2svar_inj :: Inj (=) (=) string2svar ;
 
 }.
 
 Class MLSymbols := {
   symbols : Set;
-  sym_eqdec :> EqDecision symbols;
-  sym_countable :> Countable symbols;
+  sym_eqdec :: EqDecision symbols;
+  sym_countable :: Countable symbols;
 }.
 
 Class Signature := {
-  variables :> MLVariables;
-  ml_symbols :> MLSymbols;
+  variables :: MLVariables;
+  ml_symbols :: MLSymbols;
 }.
 
 (* Later we will define signature morphisms in some file *)
