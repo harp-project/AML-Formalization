e8c56c5ec5f01066f84c4c98f6f19e50fa6d9fd6
Jan Tusil <jenda.tusil@gmail.com>
2023-11-29 18:29:54 +0100

    WIP

---
+++ examples/04_ictac23/theories/examples.v
@@ -0,0 +1,14 @@
+From MatchingLogic Require Import
+    Syntax
+    ProofSystem
+    ProofMode.MLPM
+.
+
+Section with_signature.
+    Context
+        {Σ : Signature}
+    .
+
+    Lemma A_impl_A_pm T A
+
+End with_signature.
