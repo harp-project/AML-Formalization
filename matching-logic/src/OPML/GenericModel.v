66dab504c6148778b91e50c507aa42ed18c9a7d1
Jan Tusil <jenda.tusil@gmail.com>
2023-11-30 19:02:52 +0100

    WIP generic model

---
+++ matching-logic/src/OPML/GenericModel.v
@@ -0,0 +1,262 @@
+From Coq Require Import ssreflect ssrfun ssrbool.
+
+From stdpp Require Import base list list_numbers propset.
+(* This is unset by stdpp. We need to set it again.*)
+Set Transparent Obligations.
+
+From Equations Require Import Equations.
+(* Set Equations Transparent. *)
+
+From MatchingLogic.OPML Require Import
+    OpmlSignature
+    OpmlModel
+.
+
+Definition UnifiedCarrierInfo := Type.
+
+Definition UnifiedCarrierFunctor : Type
+    := Type -> UnifiedCarrierInfo
+.
+
+Definition UnifiedCarrierCtorFamily
+    (UCF : UnifiedCarrierFunctor) (A : Type)
+: Type
+    := (nat*(list A))%type -> (* Choose a particuar constructor from the family, and arguments of the constructor from the underlying type *)
+       option (UCF A) (* maybe return something, and maybe not *)
+.
+
+Definition is_monotone (F : UnifiedCarrierFunctor)
+    : Type
+:= forall x, (x -> F x)
+.
+
+Record UnifiedCarrierComponent := {
+    ucc_functor : UnifiedCarrierFunctor ;
+    ucc_functor_monotone : is_monotone ucc_functor ;
+    ucc_ctor_family : forall A, UnifiedCarrierCtorFamily ucc_functor A ;
+}.
+
+(* We can bypass the check because of the `is_monotone` requirement. *)
+#[bypass_check(positivity=yes)]
+Inductive UnifiedCarrier
+    (F: UnifiedCarrierFunctor)
+    (pos : is_monotone F)
+: Type :=
+| c (st : F (UnifiedCarrier F pos))
+.
+
+Definition CompInvertorT
+    (UCC : UnifiedCarrierComponent)
+: Type
+    := forall (A : Type) (x : ucc_functor UCC A),
+        option ({ nla : (nat*(list A))%type | ucc_ctor_family UCC A nla = Some x })
+.
+
+Record Component := {
+    comp_UCC
+        : UnifiedCarrierComponent ;
+
+    comp_invertor
+        : CompInvertorT comp_UCC ;
+
+    comp_invertor_complete
+        : forall
+            (A : Type)
+            (nla : (nat*(list A))%type)
+            (x : ucc_functor comp_UCC A),
+            ucc_ctor_family comp_UCC A nla = Some x ->
+            exists pf,
+                comp_invertor A x = Some (exist _ nla pf)
+        ;
+}.
+
+
+Section constant.
+    Context
+        (UCI : UnifiedCarrierInfo)
+        (inh_UCI : Inhabited UCI)
+    .
+
+    Definition constant_UnifiedCarrierFunctor : UnifiedCarrierFunctor
+        := fun _ => UCI
+    .
+
+    Lemma constant_UnifiedCarrierFunctor_monotone : is_monotone constant_UnifiedCarrierFunctor.
+    Proof.
+        unfold is_monotone,constant_UnifiedCarrierFunctor,UnifiedCarrierInfo in *.
+        intros T x.
+        destruct inh_UCI as [inhabitant].
+        exact inhabitant.
+    Qed.
+
+    (* TODO *)
+    Definition constant_inversion
+        {A : Type}
+        (x : constant_UnifiedCarrierFunctor A)
+    := 0.
+
+
+End constant.
+
+Definition bool_UCC : UnifiedCarrierComponent := {|
+    ucc_functor
+        := constant_UnifiedCarrierFunctor bool ;
+
+    ucc_functor_monotone
+        := constant_UnifiedCarrierFunctor_monotone bool _ ;
+
+    ucc_ctor_family
+        := fun A nla =>
+        match nla with
+        | (0, []) => Some true
+        | (1, []) => Some false
+        | _ => None
+        end
+        ;
+|}.
+
+Program Definition bool_component : Component := {|
+    comp_UCC :=  bool_UCC ;
+    comp_invertor := fun A x =>
+        match x return option ({ nla : (nat*(list A))%type | ucc_ctor_family bool_UCC A nla = Some x }) with 
+        | true => Some (exist (fun y => ucc_ctor_family bool_UCC A y = Some true) (0,([]):(list A)) erefl)
+        | false => Some (exist _ (1,([]):(list A)) erefl)
+        end
+        ;
+|}.
+Next Obligation.
+    destruct n,l.
+    {
+        inversion H; subst; clear H.
+        exists erefl.
+        reflexivity.
+    }
+    { inversion H. }
+    {
+        destruct n; inversion H; subst.
+        {
+            exists erefl.
+            reflexivity.
+        }
+    }
+    {
+        destruct n; inversion H.
+    }
+Qed.
+Fail Next Obligation.
+
+Section paircomb.
+    Context
+        (F1 F2 : UnifiedCarrierFunctor)
+    .
+
+    Definition paircomb_UnifiedCarrierFunctor : UnifiedCarrierFunctor
+    := fun A => ((F1 A)+(F2 A))%type
+    .
+
+    Definition paircomb_type_witness (T : Type) : Type :=
+        (({A : Type | T = F1 A}) + ({A : Type | T = F2 A}))%type
+    .
+
+    Definition paircomb_inversion
+        {A : Type}
+        (x : paircomb_UnifiedCarrierFunctor A)
+        (T : Type)
+        (wit : paircomb_type_witness T)
+    :=
+    match wit with
+    | inl (exist _ A pf) => 0
+    | inr (exist _ A pf) => 0
+    end
+    .
+
+End paircomb.
+
+Section combine_ucf.
+    Context
+        (l : list (UnifiedCarrierFunctor))
+        (pf : forall (F : UnifiedCarrierFunctor), F ∈ l -> is_monotone F)
+    .
+
+    Definition combine_ucf : UnifiedCarrierFunctor
+    := fun A =>
+            let l' := (fun F => F A) <$> l in
+        foldr sum Empty_set l'
+    .
+
+End combine_ucf.
+
+Section combine.
+
+    Context
+        (l : list Component)
+    .
+
+    Definition combine : Component := {|
+        comp_UCC := 
+    |}.
+
+End combine.
+
+
+Section with_signature.
+    Context
+        {Σ : OPMLSignature}
+        (UCI : UnifiedCarrierInfo)
+        (UCF : UnifiedCarrierFunctor)
+        (pos : is_monotone UCF)
+    .
+
+    Definition CarrierFunctor : Type :=
+        forall (s : opml_sort),
+            propset UCI
+    .
+
+    Context
+        (CF : CarrierFunctor)
+        (cfi : CarrierFunctorInvertor)
+    .
+
+    
+    Definition lift_cf : propset (UnifiedCarrier UCF pos)
+    := PropSet (fun (e : UnifiedCarrier UCF pos) =>
+        match e with
+        | c _ _ x =>
+            match (cfi x) with
+            | None => False
+            | Some ((A:Type),(y:A),pf) =>
+                (* Need to know that `x` came from UCF *)
+                True
+            end
+        end
+    ).
+
+End with_signature.
+(*
+(*
+Definition ModelInfo : Type :=
+    ()%type
+.
+*)
+Record ModelInfo := {
+
+}.
+
+Definition ModelFunctor : Type -> Type :=
+    fun (CarrierBase : Type) =>
+    (Type*(ModelInfo))%type
+.
+
+Definition proj_carrier (MF : ModelFunctor) : CarrierFunctor
+:=
+    fun A => (MF A).1
+.
+*)
+Section mybool.
+    Definition F : UnifiedCarrierFunctor := fun _ => bool.
+    Definition interpretation_of_true : bool := true.
+
+
+
+End mybool.
+
