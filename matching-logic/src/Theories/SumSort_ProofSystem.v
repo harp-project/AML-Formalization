8eb169a9146b75f9eca886257a605c3fec170916
adil <adil@Ubuntu22.04.3.myguest.virtualbox.org>
2024-04-10 20:16:13 +0200

    state and prove axioms about injectivity of inject left/right symbols

--- matching-logic/src/Theories/SumSort_ProofSystem.v
+++ matching-logic/src/Theories/SumSort_ProofSystem.v
@@ -93,7 +93,136 @@ Proof.
     }
   Defined.
   
+  Theorem inject_left_inj : forall Γ , theory s1 s2 wfs1 wfs2 ⊆ Γ ->
+                              Γ ⊢ all s1, all s1, ( (b1).mlInjectL  =ml (b0).mlInjectL ) --->  
+                               (b1 =ml b0).
+  Proof.
+    intros.
+    toMLGoal.
+    clear H. wf_auto2.
+    remember (fresh_evar (s1)) as x.
+    mlIntroAllManual x.
+    cbn.
+    1:{ 
+        Search nest_ex free_evars. 
+        rewrite free_evars_nest_ex. 
+        solve_fresh.
+      }
+    mlSortedSimpl. mlSimpl. cbn.
+    
+    unfold nest_ex.
+    rewrite simpl_free_evars.
+     
+    mlIntro "H".
+    remember (fresh_evar ( patt_free_evar x ---> s1 )) as y.
+    mlIntroAllManual y.
+    1:{ 
+        cbn. solve_fresh.
+      }
+    1:{ cbn. unfold nest_ex. rewrite bevar_subst_not_occur.
+        1:{ clear H. wf_auto2. } 
+        rewrite nest_ex_aux_wfcex.
+        1:{ clear H. wf_auto2. }
+        solve_fresh.
+      }
+    mlSimpl. simpl. cbn.
+    mlIntro "H1".
+    mlIntro "H2".
+    pose proof use_sumsort_axiom AxInverseInja1 Γ H.
+    simpl in H0. mlAdd H0 as "f".
+    mlSpecialize "f" with x.
+    mlSimpl. simpl. cbn. mlRevert "f".
+    mlRewriteBy "H2" at 1.
+    mlIntro.
+    rewrite evar_open_not_occur.
+    1:{ unfold nest_ex. rewrite nest_ex_aux_wfcex. clear H. wf_auto2. clear H. wf_auto2. }
+    unfold nest_ex.
+    rewrite simpl_free_evars.
+    rewrite nest_ex_aux_wfcex.
+    1:{ clear H. wf_auto2. }
+    mlApply "0" in "H".
+    mlSymmetry in "H".
+    mlRewriteBy "H" at 1.
+    
+    mlAdd H0 as "g".
+    mlSpecialize "g" with y.
+    mlClear "H". mlClear "H2".
+    mlSimpl. simpl. cbn.
+    mlApply "g".
+    rewrite evar_open_not_occur.
+    1:{ unfold nest_ex. rewrite nest_ex_aux_wfcex. clear H. wf_auto2. clear H. wf_auto2. }
+    unfold nest_ex.
+    rewrite nest_ex_aux_wfcex.
+    1:{ clear H. wf_auto2. }
+    rewrite bevar_subst_not_occur.
+    1:{ clear H. wf_auto2. }
+    mlAssumption.
+  Defined.
   
+  Theorem inject_right_inj : forall Γ , theory s1 s2 wfs1 wfs2 ⊆ Γ ->
+                              Γ ⊢ all s2, all s2, ( (b1).mlInjectR  =ml (b0).mlInjectR ) --->  
+                               (b1 =ml b0).
+  Proof.
+    intros.
+    toMLGoal.
+    clear H. wf_auto2.
+    remember (fresh_evar (s2)) as x.
+    mlIntroAllManual x.
+    cbn.
+    1:{ 
+        rewrite free_evars_nest_ex. 
+        solve_fresh.
+      }
+    mlSortedSimpl. mlSimpl. cbn.
+    
+    unfold nest_ex.
+    rewrite simpl_free_evars.
+     
+    mlIntro "H".
+    remember (fresh_evar ( patt_free_evar x ---> s2)) as y.
+    mlIntroAllManual y.
+    1:{ 
+        cbn. solve_fresh.
+      }
+    1:{ cbn. unfold nest_ex. rewrite bevar_subst_not_occur.
+        1:{ clear H. wf_auto2. } 
+        rewrite nest_ex_aux_wfcex.
+        1:{ clear H. wf_auto2. }
+        solve_fresh.
+      }
+    mlSimpl. simpl. cbn.
+    mlIntro "H1".
+    mlIntro "H2".
+    pose proof use_sumsort_axiom AxInverseInja2 Γ H.
+    simpl in H0. mlAdd H0 as "f".
+    mlSpecialize "f" with x.
+    mlSimpl. simpl. cbn. mlRevert "f".
+    mlRewriteBy "H2" at 1.
+    mlIntro.
+    rewrite evar_open_not_occur.
+    1:{ unfold nest_ex. rewrite nest_ex_aux_wfcex. clear H. wf_auto2. clear H. wf_auto2. }
+    unfold nest_ex.
+    rewrite simpl_free_evars.
+    rewrite nest_ex_aux_wfcex.
+    1:{ clear H. wf_auto2. }
+    mlApply "0" in "H".
+    mlSymmetry in "H".
+    mlRewriteBy "H" at 1.
+    
+    mlAdd H0 as "g".
+    mlSpecialize "g" with y.
+    mlClear "H". mlClear "H2".
+    mlSimpl. simpl. cbn.
+    mlApply "g".
+    rewrite evar_open_not_occur.
+    1:{ unfold nest_ex. rewrite nest_ex_aux_wfcex. clear H. wf_auto2. clear H. wf_auto2. }
+    unfold nest_ex.
+    rewrite nest_ex_aux_wfcex.
+    1:{ clear H. wf_auto2. }
+    rewrite bevar_subst_not_occur.
+    1:{ clear H. wf_auto2. }
+    mlAssumption.
+  Defined.
   
 End sumsort.
 
