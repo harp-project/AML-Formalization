d4856adff5a1b319591768a3ec93b945d30a1052
adil <adil@Ubuntu22.04.3.myguest.virtualbox.org>
2024-05-06 17:16:20 +0200

    fix the notations module, remove local notations

--- matching-logic/src/Theories/SumSort_Syntax.v
+++ matching-logic/src/Theories/SumSort_Syntax.v
@@ -8,14 +8,12 @@ From MatchingLogic Require Import
     Theories.Sorts_Syntax
 .
 
-
 Require Import Setoid.
 From Coq.Logic Require Import Classical_Prop FunctionalExtensionality.
 From Coq.Classes Require Import Morphisms_Prop.
 Import BoundVarSugar.
 Import Definedness_Syntax.Notations.
 Import Sorts_Syntax.Notations.
-
 
 Open Scope ml_scope.
 
@@ -44,12 +42,32 @@ Class Syntax {Σ : Signature} (s1 s2 : Pattern) :=
 Module Notations.
 
 
-    Notation "'mlSum' '(' s1 ',' s2 ')'"
-        := (patt_sym (inj (ml_sum s1 s2)))
+    Notation "'mlSum' '(' s1 ',' s2 ')'" := 
+        (patt_sym (inj (ml_sum s1 s2)))
+        : ml_scope
+    .
+    
+    Notation "'(' phi ').mlInjectL(' s1 ',' s2 ')'" := 
+        (patt_app (patt_sym (inj (ml_injectL s1 s2))) phi)
+        : ml_scope
+    .
+    
+    Notation "'(' phi ').mlInjectR(' s1 ',' s2 ')'" := 
+        (patt_app (patt_sym (inj (ml_injectR s1 s2))) phi)
+        : ml_scope
+    .
+    
+    Notation "'(' phi ').mlEjectL(' s1 ',' s2 ')'" := 
+        (patt_app (patt_sym (inj (ml_ejectL s1 s2))) phi)
+        : ml_scope
+    .
+    
+    Notation "'(' phi ').mlEjectR(' s1 ',' s2 ')'" := 
+        (patt_app (patt_sym (inj (ml_ejectR s1 s2))) phi)
         : ml_scope
     .
 
-    Notation "'mlInjectL' '(' s1 ',' s2 ')' '(' phi ')'" := 
+    (* Notation "'mlInjectL' '(' s1 ',' s2 ')' '(' phi ')'" := 
         (patt_app (patt_sym (inj (ml_injectL s1 s2))) phi)
         : ml_scope
     .
@@ -67,7 +85,7 @@ Module Notations.
     Notation "'mlEjectR' '(' s1 ',' s2 ')' '(' phi ')'" := 
         (patt_app (patt_sym (inj (ml_ejectR s1 s2))) phi)
         : ml_scope
-    .
+    . *)
 
 End Notations.
 
@@ -82,34 +100,6 @@ Section axioms.
     .
 Import Notations.
 Open Scope ml_scope.
- (* Open Scope ml_scope.
-Delimit Scope ml_scope with ml. (* TODO move this somewhere else *) *)
-
-    (* Local Notation "'(' phi1 ',ml' phi2 ')'" := 
-        (patt_app (patt_app (patt_sym (inj (ml_pair s1 s2))) phi1) phi2)
-        : ml_scope
-    . *)
-
-    Local Notation "'(' phi ').mlInjectL'" := 
-        (patt_app (patt_sym (inj (ml_injectL s1 s2))) phi)
-        : ml_scope
-    .
-
-    Local Notation "'(' phi ').mlInjectR'" := 
-        (patt_app (patt_sym (inj (ml_injectR s1 s2))) phi)
-        : ml_scope
-    .
-    
-    Local Notation "'(' phi ').mlEjectL'" := 
-        (patt_app (patt_sym (inj (ml_ejectL s1 s2))) phi)
-        : ml_scope
-    .
-    
-    Local Notation "'(' phi ').mlEjectR'" := 
-        (patt_app (patt_sym (inj (ml_ejectR s1 s2))) phi)
-        : ml_scope
-    .
-
 
 
   Inductive AxiomName :=
@@ -128,93 +118,47 @@ Delimit Scope ml_scope with ml. (* TODO move this somewhere else *) *)
     | AxCoProduct
     .
 
-    (* Arguments patt_forall_of_sort {Σ self} sort phi%ml_scope. *)
-
     Definition axiom (name : AxiomName) : Pattern :=
     match name with
     
     | AxInjectLeft =>
-      all s1, ex mlSum (s1,s2) , (b1).mlInjectL =ml b0
-      (* patt_forall_of_sort s1 (
-        patt_exists_of_sort ( mlSum (s1,s2) ) (
-        
-          (b1).mlInjectL =ml b0
-        
-        )
-      )  *)
+        all s1, ex mlSum (s1,s2) , (b1).mlInjectL( s1 , s2 ) =ml b0
         
     | AxInjectRight =>
-      all s2, ex mlSum (s1,s2) , (b1).mlInjectR =ml b0 
-      (* patt_forall_of_sort s2 (
-        patt_exists_of_sort ( mlSum (s1,s2) ) (
-        
-          (b1).mlInjectR =ml b0
-        )
-      )  *)
-    
+        all s2, ex mlSum (s1,s2) , (b1).mlInjectR( s1 , s2 ) =ml b0 
     
     | AxEjectLeft =>
-      all mlSum (s1,s2), ex s1 , (b1).mlEjectL ⊆ml b0
-      (* patt_forall_of_sort ( mlSum (s1,s2) ) (
-        patt_exists_of_sort s1 (
-        
-          (b1).mlEjectL ⊆ml b0
-        
-        )
-      )  *)
+        all mlSum (s1,s2), ex s1 , (b1).mlEjectL( s1 , s2 ) ⊆ml b0
         
     | AxEjectRight =>
-      all mlSum (s1,s2), ex s2, (b1).mlEjectL ⊆ml b0
-      (* patt_forall_of_sort ( mlSum (s1,s2) ) (
-        patt_exists_of_sort s2 (
-          
-          (b1).mlEjectL ⊆ml b0
-        
-        )
-      ) *)
-        
+        all mlSum (s1,s2), ex s2, (b1).mlEjectL( s1 , s2 ) ⊆ml b0
         
     | AxInverseInja1 =>
-       all s1, ( (b0).mlInjectL ).mlEjectL =ml b0
-      (* patt_forall_of_sort s1 (
-        ( (b0).mlInjectL ).mlEjectL =ml b0 ) 
-       *)  
-       
-    | AxInverseInja2 =>
-      all s2, ( (b0).mlInjectR ).mlEjectR =ml b0
-      (* patt_forall_of_sort s2 ( 
-        ( (b0).mlInjectR ).mlEjectR =ml b0 )
- *)    
-    
-    | AxInverseInjb1 =>
-      all s2, ( (b0).mlInjectR ).mlEjectL =ml patt_bott 
-      (* patt_forall_of_sort s2 (
-        ( (b0).mlInjectR ).mlEjectL =ml patt_bott ) *)
+        all s1, ( (b0).mlInjectL( s1 , s2 ) ).mlEjectL( s1 , s2 ) =ml b0
         
-    | AxInverseInjb2 =>
-      all s1, ( (b0).mlInjectL ).mlEjectR =ml patt_bott 
-      (* patt_forall_of_sort s1 (
-        ( (b0).mlInjectL ).mlEjectR =ml patt_bott ) *)
-    
-    
-    | AxCoProduct => 
-       〚 mlSum (s1, s2) 〛  ⊆ml  patt_or ( (〚s1〛).mlInjectL )  ( (〚s2〛).mlInjectR ) 
+    | AxInverseInja2 =>
+        all s2, ( (b0).mlInjectR( s1 , s2 ) ).mlEjectR( s1 , s2 ) =ml b0
       
-(*       mlSum (s1, s2)   ⊆ml  patt_or ( (s1).mlInjectL )  ( (s2).mlInjectR )  *)
-     
+    | AxInverseInjb1 =>
+        all s2, ( (b0).mlInjectR( s1 , s2 ) ).mlEjectL( s1 , s2 ) =ml patt_bott 
+       
+    | AxInverseInjb2 =>
+        all s1, ( (b0).mlInjectL( s1 , s2 ) ).mlEjectR( s1 , s2 ) =ml patt_bott 
+      
+    | AxCoProduct => 
+        〚 mlSum (s1, s2) 〛  ⊆ml  patt_or ( (〚s1〛).mlInjectL( s1 , s2 ) )  ( (〚s2〛).mlInjectR( s1 , s2 ) ) 
     end.
 
-    Program Definition named_axioms : NamedAxioms :=
-    {| NAName := AxiomName;
-        NAAxiom := axiom;
+  Program Definition named_axioms : NamedAxioms :=
+    {| 
+      NAName := AxiomName;
+      NAAxiom := axiom;
     |}.
-    Next Obligation.
+  Next Obligation.
     destruct name; simpl; wf_auto2.
-    Qed.
+  Qed.
 
-    (* Definition Γprod := theory_of_NamedAxioms named_axioms. *)
-    Definition theory := Definedness_Syntax.theory ∪
+  Definition theory := Definedness_Syntax.theory ∪
                        theory_of_NamedAxioms named_axioms.
-
 
 End axioms.
