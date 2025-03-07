b1f3e73c3b9d0b04996f41d1d64325de6df77400
Kurucz Ádám <cphfw1@inf.elte.hu>
2025-03-07 08:28:47 +0100

    Comments removed, more things

--- matching-logic/src/OPML/doover.v
+++ matching-logic/src/OPML/doover.v
@@ -1,8 +1,9 @@
 From MatchingLogic.OPML Require Export OpmlSignature.
-From stdpp Require Import base tactics list list_numbers.
+From stdpp Require Import base tactics list list_numbers propset.
 Set Transparent Obligations.
-From Coq Require Import Program.Equality.
+From Coq Require Import Program.Equality String.
 From Equations Require Export -(notations) Equations.
+From MatchingLogic Require Import stdpp_ext.
 Set Default Proof Mode "Classic".
 
 Inductive hlist {A : Type} (F : A -> Type) : list A -> Type :=
@@ -24,104 +25,22 @@ Notation "[ ]" := hnil (format "[ ]") : hlist_scope.
 Notation "[ x ]" := (hcons x hnil) : hlist_scope.
 Notation "[ x ; y ; .. ; z ]" := (hcons x (hcons y .. (hcons z hnil) ..)) (format "[ '[' x ; '/' y ; '/' .. ; '/' z ']' ]") : hlist_scope.
 
-(* Check [1; 2; true; 3; false]%hlist : hlist (bool_rect _ nat bool) [true; true; false; true; false]. *)
+Tactic Notation "hlist_map" uconstr(f) "in" ident(h) :=
+  let y := fresh in let H := fresh in
+  induction h as [ | ? ? y _ H];
+  [left | right];
+  [eapply f; eauto using y | exact H].
 
-(* Definition hlistIn {A : Type} {ED : EqDecision A} {F : A -> Type} {x : A} {xs : list A} (y : F x) (ys : hlist F xs) : Prop := *)
-(*   match ys with *)
-(*   | hnil => False *)
-(*   | @hcons _ _ x' _ y' ys' => match (decide (x = x')) with *)
-(*                               | left e => (eq_rect _ _ y _ e = y') *)
-(*                               | _ => False *)
-(*                               end *)
-(*   end. *)
-
-(* Arguments hlistIn {_} {_} {_} {_} {_} & _ !_. *)
+Tactic Notation "hlist_foldr" uconstr(f) "with" uconstr(def) "in" ident(h) :=
+  let y := fresh in let H := fresh in
+  induction h as [ | ?x ?xs y _ H ];
+  [exact def | eapply f];
+  [exact y | exact H].
 
 Inductive Dbi {Σ : OPMLSignature} : opml_sort -> list opml_sort -> Type :=
   | dbiz {x} {xs} : Dbi x (x :: xs)
   | dbis {x y} {xs} : Dbi y xs -> Dbi y (x :: xs)
 .
-
-(* Fixpoint dbi_depth {Σ : OPMLSignature} {s} {xs} (x : Dbi s xs) : nat := *)
-(*   match x with *)
-(*   | dbiz => 0 *)
-(*   | dbis e => 1 + dbi_depth e *)
-(*   end. *)
-
-(* Arguments dbi_depth {_} {_} {_} !_. *)
-
-Inductive OPMLPattern {Σ : OPMLSignature} : opml_sort -> list opml_sort -> list opml_sort -> Type :=
-  | op_bot {s} {ex mu} : OPMLPattern s ex mu
-  | op_bevar {s} {ex mu} : Dbi s ex -> OPMLPattern s ex mu
-  | op_app {ex mu} (σ : opml_symbol) : hlist (OPMLPattern ^~ ex ^~ mu) (opml_arg_sorts σ) -> OPMLPattern (opml_ret_sort σ) ex mu
-  | op_ex {s s'} {ex mu} : OPMLPattern s (s' :: ex) mu -> OPMLPattern s ex mu
-.
-
-(* Fixpoint depth {Σ : OPMLSignature} {s : opml_sort} {ex mu : list opml_sort} (x : OPMLPattern s ex mu) : nat := *)
-(*   match x with *)
-(*   | op_bot => 0 *)
-(*   | op_bevar e => dbi_depth e *)
-(*   | op_app σ l => hlist_rect 0 (fun _ _ y _ z => 1 + depth y + z) l *)
-(*   | op_ex φ => 1 + depth φ *)
-(*   end. *)
-
-(* Arguments depth {_} {_} {_} {_} !_. *)
-
-(* Definition expat {Σ : OPMLSignature} : Type := sigT (uncurry3 OPMLPattern). *)
-
-(* Definition SubTree {Σ : OPMLSignature} : relation (sigT (uncurry3 OPMLPattern)). *)
-(* Proof. *)
-(*   intros [[[s ex] mu] x] [[[s' ex'] mu'] y]. *)
-(*   simpl in x, y. *)
-(*   destruct_with_eqn y. *)
-(*   exact False. *)
-(*   exact False. *)
-(*   refine (match decide ((ex, mu) = (ex0, mu0)) with *)
-(*           | left e => let u := eq_rect (ex, mu) (uncurry (OPMLPattern s)) x _ e : OPMLPattern s ex0 mu0 in hlistIn u h *)
-(*           | _ => False *)
-(*           end). *)
-(*   refine (match decide ((s, ex, mu) = (s0, s' :: ex0, mu0)) with *)
-(*           | left e => eq_rect (s, ex, mu) (uncurry3 OPMLPattern) x _ e = o *)
-(*           | _ => False *)
-(*           end). *)
-(* Defined. *)
-
-(* Arguments SubTree {_} _ _ /. *)
-
-(*Goal forall {Σ : OPMLSignature}, well_founded SubTree. *)
-(*Proof. *)
-(*  intros. *)
-(*  unfold well_founded. *)
-(*  intros [[[s ex] mu] a]. simpl in a. *)
-(*  revert s ex mu a. *)
-(*  fix IH 4. *)
-(*  intros ? ? ? []. *)
-(*  split. intros [[[s' ex'] mu'] y] H. simpl in H. destruct H. *)
-(*  split. intros [[[s' ex'] mu'] y] H. simpl in H. destruct H. *)
-(*  split. intros [[[s' ex'] mu'] y] H. simpl in H. *)
-(*  case_match. 2: destruct H. *)
-(*  destruct h. simpl in H. destruct H. *)
-(*  simpl in H. case_match. 2: destruct H. *)
-(*  assert (existT (s', ex', mu') y = existT (x, ex0, mu0) o). { *)
-(*    eapply eq_existT_curried. *)
-(*    Search eq_rect. *)
-(*    admit. *)
-(*  } *)
-(*  rewrite H2. *)
-(*  apply IH. Guarded. *)
-(*  split. intros [[[s'' ex'] mu'] y] H. simpl in H. *)
-(*  case_match. 2: destruct H. *)
-(*  epose proof eq_existT_curried e H. rewrite H1. *)
-(*  apply IH. Guarded. *)
-(*Abort. *)
-
-(* Inductive sub {Σ : OPMLSignature} : list opml_sort -> list opml_sort -> list opml_sort -> Type := *)
-(*   | subp {xs mu : list opml_sort} {x : opml_sort} : sub (x :: xs) xs mu *)
-(*   | subz {xs mu : list opml_sort} {x : opml_sort} : OPMLPattern x xs mu -> sub xs (x :: xs) mu *)
-(*   | subs {xs ys mu : list opml_sort} {x : opml_sort} : sub xs ys mu -> sub (x :: xs) (x :: ys) mu *)
-(*   . *)
-
-(* Check subs (subs (subs (subz op_bot))). *)
 
 Fixpoint inc_dbi {Σ : OPMLSignature} {s} {ex ex' ex''} (dbi : Dbi s (ex ++ ex'')) : Dbi s (ex ++ ex' ++ ex'').
 Proof.
@@ -143,13 +62,15 @@ Defined.
 
 Arguments inc_dbi {_} {_} {_} {_} {_} !_.
 
-(* Goal forall {Σ : OPMLSignature} {s s0 s1 s2 s3 s4 : opml_sort} , Dbi s [s0; s1; s4] -> Dbi s [s0; s1; s2; s3; s4]. *)
-(* Proof. *)
-(*   intros. *)
-(*   pose (@inc_dbi _ s [s0; s1] [s2; s3] [s4]). *)
-(*   simpl in d. *)
-(*   apply d, H. *)
-(* Qed. *)
+Inductive OPMLPattern {Σ : OPMLSignature} : opml_sort -> list opml_sort -> list opml_sort -> Type :=
+  | op_bot {s} {ex mu} : OPMLPattern s ex mu
+  | op_bevar {s} {ex mu} : Dbi s ex -> OPMLPattern s ex mu
+  | op_fevar {s} {ex mu} : opml_evar s -> OPMLPattern s ex mu
+  | op_imp {s} {ex mu} : OPMLPattern s ex mu -> OPMLPattern s ex mu -> OPMLPattern s ex mu
+  | op_app {ex mu} (σ : opml_symbol) : hlist (OPMLPattern ^~ ex ^~ mu) (opml_arg_sorts σ) -> OPMLPattern (opml_ret_sort σ) ex mu
+  | op_eq {s s'} {ex mu} : OPMLPattern s ex mu -> OPMLPattern s ex mu -> OPMLPattern s' ex mu
+  | op_ex {s s'} {ex mu} : OPMLPattern s (s' :: ex) mu -> OPMLPattern s ex mu
+.
 
 Fixpoint inc_evar {Σ : OPMLSignature} {s} {ex ex' ex'' mu} (base : OPMLPattern s (ex ++ ex'') mu) : OPMLPattern s (ex ++ ex' ++ ex'') mu.
 Proof.
@@ -159,12 +80,13 @@ Proof.
   dependent destruction base.
   - exact op_bot.
   - apply op_bevar, inc_dbi, d.
+  - apply op_fevar, o.
+  - apply op_imp; apply inc_evar.
+    exact base1. exact base2.
   - apply op_app.
-    induction h; simpl.
-    + left.
-    + right.
-      * apply inc_evar, f.
-      * apply IHh.
+    hlist_map inc_evar in h.
+  - eapply op_eq; apply inc_evar.
+    exact base1. exact base2.
   - eapply op_ex. instantiate (1 := s').
     rewrite app_comm_cons.
     apply inc_evar.
@@ -191,20 +113,207 @@ Arguments sub_evar {_} {_} {_} {_} {_} {_} !_ _.
 Fixpoint bevar_subst {Σ : OPMLSignature} {s s'} {ex ex' mu} (base : OPMLPattern s (ex ++ s' :: ex') mu) (repl : OPMLPattern s' ex' mu) {struct base} : OPMLPattern s (ex ++ ex') mu.
 Proof.
   intros.
-  (* remember base. *)
   dependent destruction base.
   - exact op_bot.
   - eapply sub_evar; eauto.
+  - apply op_fevar, o.
+  - apply op_imp; eapply bevar_subst.
+    2,4: exact repl. exact base1. exact base2.
   - apply op_app.
-    induction h.
-    + left.
-    + right.
-      * eapply bevar_subst; eauto.
-      * apply IHh.
+    hlist_map bevar_subst in h.
+  - eapply op_eq; eapply bevar_subst.
+    2,4: exact repl. exact base1. exact base2.
   - eapply op_ex.
     rewrite app_comm_cons.
     eapply bevar_subst.
     rewrite <- app_comm_cons. all: eauto.
 Defined.
 
-Arguments bevar_subst {_} {_} {_} {_} {_} {_} !_ _.
+Arguments bevar_subst {_} {_} {_} _ {_} {_} !_ _.
+
+Module Example.
+Inductive sorts := nat_s | bool_s.
+Inductive symbols := add_s | is0_s | zero_s | succ_s | true_s | false_s.
+
+Fixpoint pos_idx {A : Type} (l : list A) (p : positive) {struct l} : option A :=
+  match p, l with
+  | 1%positive, (x :: xs) => Some x
+  | 1%positive, [] => None
+  | p, (x :: xs) => pos_idx xs (Pos.pred p)
+  | _, [] => None
+  end.
+
+Instance NatBool : OPMLSignature.
+Proof.
+  unshelve esplit.
+  unshelve esplit.
+  exact sorts. exact (λ x y, x = y).
+  unfold EqDecision, Decision. decide equality.
+  unshelve esplit. exact (sorts_rect _ 1%positive 2%positive).
+  exact (pos_idx [nat_s; bool_s]).
+  simpl. intros []; auto.
+  split. split. auto. intros ? ? ? ? ?. rewrite <- H0. exact H.
+  intros ? ? ? ?. exact H.
+  unshelve esplit.
+  1,2: exact (const string).
+  1-6: typeclasses eauto.
+  unshelve esplit.
+  exact symbols.
+  unfold EqDecision, Decision. decide equality.
+  unshelve esplit.
+  exact (symbols_rect _ 1%positive 2%positive 3%positive 4%positive 5%positive 6%positive).
+  exact (pos_idx [add_s; is0_s; zero_s; succ_s; true_s; false_s]).
+  intros []; auto.
+  exact (symbols_rect _ [nat_s; nat_s] [nat_s] [] [nat_s] [] []).
+  exact (symbols_rect _ nat_s bool_s nat_s nat_s bool_s bool_s).
+Defined.
+
+End Example.
+
+Section asd.
+  Context {Σ : OPMLSignature}.
+
+  Record OPMLModel := {
+    opml_carrier :> opml_sort -> Set;
+    opml_app (σ : opml_symbol) :
+    hlist opml_carrier (opml_arg_sorts σ) -> propset (opml_carrier (opml_ret_sort σ));
+    opml_inhabited (s : opml_sort) : Inhabited (opml_carrier s)
+  }.
+
+  Context {M : OPMLModel}.
+
+  Record OPMLValuation : Type := {
+    evar_valuation {s : opml_sort} (x : opml_evar s) : opml_carrier M s;
+    svar_valuation {s : opml_sort} (X : opml_svar s) : propset (opml_carrier M s) ;
+  }.
+
+  (* Definition cons_eq_inv {A} {x y : A} {xs ys} (H : x :: xs = y :: ys) : x = y /\ xs = ys := conj (f_equal (list_rect _ x (λ a _ _, a)) H) (f_equal (list_rect _ xs (λ _ a _, a)) H). *)
+
+  Definition hcons_inv {A} {F : A -> Type} {x} {xs} P (X : forall (y : F x) (ys : hlist F xs), P) (ys : hlist F (x :: xs)) : P :=
+    match ys in (hlist _ l) return (l = x :: xs -> P) with
+    | hnil => λ H, False_rect _ (eq_ind nil (list_rect _ True (λ _ _ _, False)) I _ H)
+    | @hcons _ _ a l y ys => λ H, X (eq_rect a F y x (f_equal (list_rect _ x (λ a _ _, a)) H)) (eq_rect l (hlist F) ys xs (f_equal (list_rect _ xs (λ _ a _, a)) H))
+    end eq_refl.
+
+  Fixpoint pointwise_elem_of {A} {F : A -> Type} {ss} (l : hlist F ss) (lps : hlist (propset ∘ F) ss) {struct ss} : Prop :=
+    match ss return (hlist F ss -> hlist (propset ∘ F) ss -> Prop) with
+    | [] => λ _ _, True
+    | (x :: xs) => λ l lps, hcons_inv _ (λ y ys, hcons_inv _ (λ y' ys', y ∈ y' /\ pointwise_elem_of ys ys') lps) l
+    end l lps.
+
+  Arguments pointwise_elem_of {_} {_} {_} & _ _ /.
+
+  Definition test1 : hlist (bool_rect _ nat bool) [true; false] := [1; true].
+  Definition test2 : hlist (propset ∘ bool_rect _ nat bool) [true; false] := [{[1]}; {[true]}].
+  Goal pointwise_elem_of test1 test2.
+  Proof.
+    simpl. set_solver.
+  Qed.
+
+  Definition app_ext (σ : opml_symbol) (args : hlist (propset ∘ (opml_carrier M)) (opml_arg_sorts σ)) : propset (opml_carrier M (opml_ret_sort σ)) := PropSet (fun e => exists (l : hlist (opml_carrier M) (opml_arg_sorts σ)), pointwise_elem_of l args /\ e ∈ opml_app M σ l).
+
+  Definition update_evar_val {s} (ev : opml_evar s) (x : opml_carrier M s) (val : OPMLValuation) : OPMLValuation.
+  Proof.
+    unshelve esplit.
+    intros s' ev'.
+    destruct (decide (s = s')).
+    destruct (decide (eq_rect s opml_evar ev _ e = ev')).
+    exact (eq_rect s M x _ e).
+    1,2:exact (evar_valuation val ev').
+    exact (@svar_valuation val).
+  Defined.
+
+  (* Fixpoint map_hlist {A : Type} {F G : A -> Type} {l : list A} (f : forall (a : A), F a -> G a) (xs : hlist F l) {struct l} : hlist G l. *)
+  (* Proof. *)
+  (*   induction l. *)
+  (*   left. *)
+  (*   inversion xs; subst. *)
+  (*   right. *)
+  (*   apply f, X. *)
+  (*   apply IHl, X0. *)
+  (* Defined. *)
+
+  Fixpoint free_evars {s} {ex mu} (sTarget : opml_sort) (φ : OPMLPattern s ex mu) : gset (opml_evar sTarget) :=
+    match φ in (OPMLPattern s' _ _) return (s = s' -> gset (opml_evar sTarget)) with
+    | op_bot | op_bevar _ => λ _, empty
+    | @op_fevar _ o _ _ x => λ H, match decide (s = sTarget) with
+                                   | left e => {[eq_rect o opml_evar x sTarget (eq_trans (eq_sym H) e)]}
+                                   | _ => empty
+                                   end
+    | op_imp φ1 φ2 | op_eq φ1 φ2 => λ _, free_evars sTarget φ1 ∪ free_evars sTarget φ2
+    | op_app σ l => λ _, ltac:(hlist_foldr (union ∘ free_evars _ _ _ sTarget) with empty in l)
+    | op_ex x => λ _, free_evars sTarget x
+    end eq_refl.
+
+  Definition fresh_evar {s} {ex mu} (sTarget : opml_sort) (φ : OPMLPattern s ex mu) : opml_evar sTarget := fresh (elements (free_evars sTarget φ)).
+  
+  Fixpoint opml_size {s} {ex mu} (φ : OPMLPattern s ex mu) : nat :=
+    match φ with
+    | op_bot | op_bevar _ | op_fevar _ => 1
+    | op_imp φ1 φ2 | op_eq φ1 φ2 => S (opml_size φ1 + opml_size φ2)
+    | op_app σ l => ltac:(hlist_foldr (plus ∘ opml_size _ _ _) with 0 in l)
+    | op_ex φ => S (opml_size φ)
+    end.
+
+  Arguments opml_size {_} {_} {_} & !_.
+
+  Obligation Tactic := idtac.
+
+  Fixpoint hlistIn {A} {F : A -> Type} {x} {xs} {ED : EqDecision A} (y : F x) (ys : hlist F xs) : Prop.
+  Proof.
+    destruct ys.
+    exact False.
+    destruct (decide (x = x0)).
+    exact (eq_rect x F y x0 e = f \/ hlistIn _ _ _ _ _ y ys).
+    exact False.
+  Defined.
+
+  Arguments hlistIn {_} {_} {_} {_} {_} & _ !_.
+
+  Program Fixpoint opml_eval {ex mu} (ρ : OPMLValuation) {s} (φ : OPMLPattern s ex mu) {measure (opml_size φ)} : propset (opml_carrier M s) :=
+    match φ with
+    | op_bot | op_bevar _ => empty
+    | op_fevar x => {[evar_valuation ρ x]}
+    | op_imp φ1 φ2 => (⊤ ∖ (opml_eval ρ φ1)) ∪ (opml_eval ρ φ2)
+    | op_app σ l => app_ext σ _
+    | op_eq φ1 φ2 => PropSet (λ _, (opml_eval ρ φ1) = (opml_eval ρ φ2))
+    | @op_ex _ _ s' _ _ φ => propset_fa_union (λ X, let o := fresh_evar s' φ in opml_eval (update_evar_val o X ρ) (bevar_subst [] φ (op_fevar o)))
+    end.
+
+  Next Obligation.
+    intros. subst. simpl. lia.
+  Defined.
+  Next Obligation.
+    intros. subst. simpl. lia.
+  Defined.
+  Next Obligation.
+    intros. subst. simpl in opml_eval.
+  Admitted.
+  Next Obligation.
+    intros. subst. simpl. lia.
+  Defined.
+  Next Obligation.
+    intros. subst. simpl. lia.
+  Defined.
+  Lemma bevar_subst_opml_size : forall s s' ex mu (φ : OPMLPattern s (s' :: ex) mu) x, opml_size (bevar_subst [] φ (op_fevar x)) = opml_size φ.
+  Proof.
+    intros.
+    dependent induction φ; simpl; auto.
+    * erewrite <- IHφ1, <- IHφ2. simpl.
+    * f_equal. induction H; simpl.
+      reflexivity.
+      rewrite IHForall.
+      erewrite H.
+      reflexivity.
+    * by erewrite IHφ.
+    * by erewrite IHφ.
+    * by rewrite ->IHφ1, ->IHφ2.
+  Defined.
+  Next Obligation.
+    intros. subst. simpl.
+    clear. subst o.
+    induction φ; simpl.
+  Admitted.
+  Next Obligation.
+    apply measure_wf, lt_wf.
+  Defined.
